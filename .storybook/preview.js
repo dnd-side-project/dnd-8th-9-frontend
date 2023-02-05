50ab0868dbc77254abf8ea2e2ba19a5cc42b8add
rachel490 <ccori12@naver.com>
2023-02-06 01:22:42 +0900

    chore: storybook next.js image de-optimization 추가

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,12 @@
+import * as NextImage from "next/image";
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: props => <OriginalNextImage {...props} unopitimized />,
+});
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
