deb44d52bc28c6dbabafd3a87459c2191c93a3da
rachel490 <ccori12@naver.com>
2023-02-06 17:57:44 +0900

    feat: tanstack-query 예제 추가

--- components/Task/Task.tsx
+++ components/Task/Task.tsx
@@ -1,10 +1,17 @@
 import { useTheme } from "@emotion/react";
+import { useQuery } from "@tanstack/react-query";
+
 import useBearStore from "@/store/store";
+import fetchUser from "@/api/api";
 import Paragraph from "./Task.styled";
 
 export default function Task() {
   const { colors } = useTheme();
   const { bears, increase, decrease } = useBearStore(state => state);
+  const { isLoading, data: users, isError } = useQuery(["users"], fetchUser);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Error</p>;
 
   return (
     <>
@@ -19,6 +26,11 @@ export default function Task() {
         <button type="button" onClick={decrease}>
           -
         </button>
+      </div>
+      <div>
+        {users.map(user => (
+          <p key={user.id}>{user.name}</p>
+        ))}
       </div>
     </>
   );
