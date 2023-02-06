5d6ba14713675c0f1c84ecd94d50794584b9187a
rachel490 <ccori12@naver.com>
2023-02-06 16:36:09 +0900

    style: theme - fontsize, color 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "@emotion/react";
+
 import GlobalStyle from "@/styles/Global";
+import theme from "@/styles/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
---
+++ styles/theme.ts
@@ -0,0 +1,36 @@
+import { Theme } from "@emotion/react";
+
+const theme: Theme = {
+  fontSizes: {
+    10: "10px",
+    12: "12px",
+    14: "14px",
+    16: "16px",
+    18: "18px",
+    20: "20px",
+    22: "22px",
+    24: "24px",
+    26: "26px",
+    28: "28px",
+    30: "30px",
+    32: "32px",
+  },
+  colors: {
+    primary: "#F1424D",
+    secondary: "#FFA4A9",
+    dark: {
+      100: "#7C87AD",
+      300: "#4C536D",
+      500: "#0E1B47",
+    },
+    gray: {
+      100: "#D7D7D7",
+      300: "#A5A5A5",
+      500: "#3C3C3C",
+    },
+    black: "#000",
+    white: "#fff",
+  },
+};
+
+export default theme;
