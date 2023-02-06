0823056a7ae44789c98a5a0f687b4e040d0d5df2
rachel490 <ccori12@naver.com>
2023-02-06 17:57:16 +0900

    chore: tanstack-query 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,21 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@emotion/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import GlobalStyle from "@/styles/Global";
 import theme from "@/styles/theme";
 
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+        <ReactQueryDevtools position="bottom-right" initialIsOpen={false} />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
