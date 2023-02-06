e7d22630bb47cd1edb0b1be74d632389451bd23e
rachel490 <ccori12@naver.com>
2023-02-06 16:38:47 +0900

    style: theme 사용 예제 추가

--- components/Task/Task.tsx
+++ components/Task/Task.tsx
@@ -1,7 +1,12 @@
+import { useTheme } from "@emotion/react";
 import Paragraph from "./Task.styled";
 
-export default function Home() {
+export default function Task() {
+  const { colors } = useTheme();
+
   return (
-    <Paragraph hoverColor="red">I am a component made with Emotion (Styled Components)</Paragraph>
+    <Paragraph hoverColor={colors.secondary}>
+      I am a component made with Emotion (Styled Components)
+    </Paragraph>
   );
 }
