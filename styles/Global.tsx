41f2b226d715941d4eece77b2310abfc0abe2759
rachel490 <ccori12@naver.com>
2023-02-06 15:25:42 +0900

    chore: global style 추가

---
+++ styles/Global.tsx
@@ -0,0 +1,63 @@
+import { Global, css } from "@emotion/react";
+
+export const supportDeviceSize = 768;
+
+const style = css`
+  html {
+    font-size: 62.5%;
+
+    @media all and (max-width: ${supportDeviceSize}px) {
+      font-size: 31.25%;
+    }
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-size: 1.6rem;
+    line-height: 2.4rem;
+  }
+
+  button {
+    border: none;
+    background-color: transparent;
+    width: 100%;
+    height: 100%;
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+    cursor: pointer;
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+
+  input {
+    border: none;
+    outline: none;
+  }
+
+  /* Chrome, Safari, Edge, Opera */
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  /* Firefox */
+  input[type="number"] {
+    -moz-appearance: textfield;
+  }
+`;
+
+const GlobalStyle = () => {
+  return <Global styles={style} />;
+};
+
+export default GlobalStyle;
