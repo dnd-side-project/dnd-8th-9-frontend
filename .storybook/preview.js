5e26391c4d9fa659b75ef869a9c9958394adc2dd
rachel490 <ccori12@naver.com>
2023-02-10 08:57:40 +0900

    fix: storybook에emotion의 GlobalStyle & theme 적용

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,8 @@
+import { ThemeProvider } from "@emotion/react";
 import * as NextImage from "next/image";
+
+import theme from "../styles/theme";
+import GlobalStyle from "../styles/Global";
 
 const OriginalNextImage = NextImage.default;
 
@@ -6,6 +10,15 @@ Object.defineProperty(NextImage, "default", {
   configurable: true,
   value: props => <OriginalNextImage {...props} unopitimized />,
 });
+
+export const decorators = [
+  Story => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Story />
+    </ThemeProvider>
+  ),
+];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
