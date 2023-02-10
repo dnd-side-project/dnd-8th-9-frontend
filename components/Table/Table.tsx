020faf859cd1c45b31cb1106d4867d042f0e9c9f
Serin-Kim <serin9864@gmail.com>
2023-02-10 11:08:38 +0900

    refactor: Table 컴포넌트 통합
    InfoTable과 NoticeTable을 하나의 Table 컴포넌트로 사용할 수 있게 리팩터링 함
    
    Co-authored-by: Rachel Shin (dwell) <rachel490@users.noreply.github.com>

---
+++ components/Table/Table.tsx
@@ -0,0 +1,40 @@
+import * as S from "./Table.styled";
+
+interface IContents {
+  option: string;
+  data: ({ day: string; hour: string } | { key: string; value: string })[];
+}
+
+interface IEl {
+  [key: string]: string;
+}
+
+interface IOptions {
+  [key: string]: { [key: string]: string };
+}
+
+const optionList: IOptions = {
+  notice: {
+    key: "day",
+    value: "hour",
+  },
+  info: {
+    key: "key",
+    value: "value",
+  },
+};
+
+export default function Table({ option, data }: IContents) {
+  const { key, value } = optionList[option];
+
+  return (
+    <>
+      {data.map((el: IEl) => (
+        <S.Container key={el[key]}>
+          <S.Cell option={option}>{el[key]}</S.Cell>
+          <S.Cell option={option}>{el[value]}</S.Cell>
+        </S.Container>
+      ))}
+    </>
+  );
+}
