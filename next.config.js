20b804d2e6208d71b7dd1ab30096f4811df3f2b2
Serin-Kim <serin9864@gmail.com>
2023-02-13 01:02:16 +0900

    chore: svg 설정

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com"],
   },
+  webpack: config => {
+    // 아래를 추가합니다.
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
