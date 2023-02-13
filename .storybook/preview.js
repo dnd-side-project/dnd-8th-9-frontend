6cff6fcdaba58b54c6f45e2fd2befb74d2e605b9
rachel490 <ccori12@naver.com>
2023-02-13 22:22:03 +0900

    fix: storybook에 tanstack query 적용

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,6 @@
-import { ThemeProvider } from "@emotion/react";
 import * as NextImage from "next/image";
+import { ThemeProvider } from "@emotion/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import theme from "../styles/theme";
 import GlobalStyle from "../styles/Global";
@@ -8,11 +9,15 @@ NextImage.defaultProps = {
   unoptimized: true,
 };
 
+const queryClient = new QueryClient();
+
 export const decorators = [
   Story => (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Story />
+      <QueryClientProvider client={queryClient}>
+        <Story />
+      </QueryClientProvider>
     </ThemeProvider>
   ),
 ];
