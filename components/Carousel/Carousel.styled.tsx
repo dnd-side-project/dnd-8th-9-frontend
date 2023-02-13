227658280a3b2c5d65e87b4f7839d4021ad5531a
Serin-Kim <serin9864@gmail.com>
2023-02-14 00:53:17 +0900

    style: Carousel css 추가

--- components/Carousel/Carousel.styled.tsx
+++ components/Carousel/Carousel.styled.tsx
@@ -4,11 +4,13 @@ export const Container = styled.div`
   img {
     width: 32rem;
     height: 27rem;
+    border-radius: 8px;
   }
   .swiper-pagination-bullet {
     background-color: ${({ theme }) => theme.colors.white};
   }
   .swiper-pagination-bullet-active {
-    background-color: ${({ theme }) => theme.colors.gray[300]};
+    background-color: ${({ theme }) => theme.colors.black};
   }
+  margin: 12px 0 12px 0;
 `;
