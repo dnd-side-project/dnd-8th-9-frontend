14d8bc909444f2033835dd8449a73c71e2de4486
rachel490 <ccori12@naver.com>
2023-02-13 20:13:09 +0900

    fix: storybook에 next.js path alias 설정 추가 (build error 해결)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
 module.exports = {
   stories: ["../**/*.stories.mdx", "../**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -8,5 +10,15 @@ module.exports = {
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-webpack5",
+  },
+  webpackFinal: async config => {
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        extensions: config.resolve.extensions,
+      }),
+    ];
+
+    return config;
   },
 };
