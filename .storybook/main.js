43a6f6fb0e2fc79e96a41d24b937fe94ddb229cc
rachel490 <ccori12@naver.com>
2023-02-13 20:14:24 +0900

    fix: storybook에 next.js svgr 적용 (storybook svg error 해결)

--- .storybook/main.js
+++ .storybook/main.js
@@ -19,6 +19,15 @@ module.exports = {
       }),
     ];
 
+    const fileLoaderRule = config.module.rules.find(rule => rule.test && rule.test.test(".svg"));
+    fileLoaderRule.exclude = /\.svg$/;
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      enforce: "pre",
+      loader: require.resolve("@svgr/webpack"),
+    });
+
     return config;
   },
 };
