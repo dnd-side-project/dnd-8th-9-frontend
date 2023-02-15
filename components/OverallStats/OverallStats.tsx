86713cd78712442461cbc27c02646859c3244e9d
Serin-Kim <serin9864@gmail.com>
2023-02-15 14:30:56 +0900

    feat: OverallStats 컴포넌트 초기 설정

---
+++ components/OverallStats/OverallStats.tsx
@@ -0,0 +1,5 @@
+import * as S from "./OverallStats.styled";
+
+export default function OverallStats() {
+  return <S.Container>this is overallStats</S.Container>;
+}
--- pages/store/[storeId]/review/index.tsx
+++ pages/store/[storeId]/review/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { reviews } from "@/mocks/mockData/review"; // TODO: msw에서 받아와야 함
 import Review from "@/components/Review/Review";
+import OverallStats from "@/components/OverallStats/OverallStats";
 
 function ReviewPage() {
   const review = reviews.reviewList[0];
 
-  return <Review review={review} />;
+  return (
+    <>
+      <OverallStats />
+      <Review review={review} />
+    </>
+  );
 }
 
 export default ReviewPage;
