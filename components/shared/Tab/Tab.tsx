538bd312af1ddc72aa8a19579f6ce3bfc6e08783
rachel490 <ccori12@naver.com>
2023-02-16 19:04:20 +0900

    feat: tab 컴포넌트 구현

---
+++ components/shared/Tab/Tab.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import { SerializedStyles, Theme } from "@emotion/react";
+import { tab, tabType } from "./tab.styled";
+
+export interface ITabItem {
+  label: string;
+  link: string;
+}
+
+export interface IProp {
+  menuList: ITabItem[];
+  type: "swipeable" | "fixed";
+  cssProp?: SerializedStyles | (({ colors, fontSizes }: Theme) => SerializedStyles);
+}
+
+function Tab({ menuList, type, cssProp }: IProp) {
+  const [currentMenu, setCurrentMenu] = useState(menuList[0].label);
+  const { asPath } = useRouter();
+
+  const handleClick = (menu: string) => {
+    setCurrentMenu(menu);
+  };
+
+  return (
+    <ul role="tablist" css={[tab, tabType[type], cssProp]}>
+      {menuList.map(menu => (
+        <li
+          key={menu.label}
+          role="presentation"
+          className={menu.label === currentMenu ? "isSelected" : ""}
+        >
+          <a
+            role="tab"
+            tabIndex={0}
+            aria-selected={currentMenu === menu.label}
+            href={`${asPath}${menu.link}`}
+            onClick={() => handleClick(menu.label)}
+          >
+            {menu.label}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default Tab;
