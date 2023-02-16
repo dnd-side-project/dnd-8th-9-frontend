47bfda81dfb74340d776414b1d45b13dfe924e7b
rachel490 <ccori12@naver.com>
2023-02-16 19:03:30 +0900

    feat: tab navigation constants 추가

---
+++ constants/navigations.ts
@@ -0,0 +1,91 @@
+export const storeTab = [
+  {
+    label: "메뉴",
+    link: "/menu",
+  },
+  {
+    label: "주문양식",
+    link: "/order",
+  },
+  {
+    label: "리뷰",
+    link: "/review",
+  },
+  {
+    label: "공지사항",
+    link: "/notice",
+  },
+  {
+    label: "정보",
+    link: "/info",
+  },
+];
+
+export const storeMenuTab = [
+  {
+    label: "사이즈",
+    name: "size",
+    link: "#size",
+  },
+  {
+    label: "맛",
+    link: "#flavor",
+  },
+  {
+    label: "모양 변경",
+    link: "#variation",
+  },
+  {
+    label: "주의사항",
+    link: "#caution",
+  },
+  {
+    label: "리뷰",
+    link: "#review",
+  },
+];
+
+export const resultTab = [
+  {
+    label: "메뉴",
+    link: "/menu",
+  },
+  {
+    label: "스토어",
+    link: "/store",
+  },
+];
+
+export const homeTab = [
+  {
+    label: "당도추천",
+    link: "/dango",
+  },
+  {
+    label: "스토어",
+    link: "/store",
+  },
+  {
+    label: "리얼리뷰",
+    link: "/review",
+  },
+];
+
+export const categoryTab = [
+  {
+    label: "레터링",
+    link: "#",
+  },
+  {
+    label: "캐릭터/입체",
+    link: "#",
+  },
+  {
+    label: "포토",
+    link: "#",
+  },
+  {
+    label: "꽃",
+    link: "#",
+  },
+];
