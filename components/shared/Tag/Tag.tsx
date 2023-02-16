1d430db5ed73058ad6bb0e35bc74afeddc8d2a9c
rachel490 <ccori12@naver.com>
2023-02-17 04:26:38 +0900

    feat: Tag 컴포넌트 구현

---
+++ components/shared/Tag/Tag.tsx
@@ -0,0 +1,19 @@
+import { SerializedStyles, Theme } from "@emotion/react";
+import { tag, tagStyle } from "./tag.styled";
+
+export interface IProp {
+  type: "square" | "icon" | "single" | "double";
+  children: string | JSX.Element | JSX.Element[];
+  label: string;
+  cssProp?: SerializedStyles | (({ colors }: Theme) => SerializedStyles);
+}
+
+function Tag({ type, children, label, cssProp }: IProp) {
+  return (
+    <div aria-label={label} css={[tag, tagStyle[type], cssProp]}>
+      {children}
+    </div>
+  );
+}
+
+export default Tag;
