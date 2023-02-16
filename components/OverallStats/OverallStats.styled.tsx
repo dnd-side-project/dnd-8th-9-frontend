637fffd90bd86035003c5fa586d345b5b94ec2a3
Serin-Kim <serin9864@gmail.com>
2023-02-16 17:10:40 +0900

    feat: OverallStats 컴포넌트 구현

--- components/OverallStats/OverallStats.styled.tsx
+++ components/OverallStats/OverallStats.styled.tsx
@@ -1,9 +1,72 @@
 import styled from "@emotion/styled";
 
 export const Container = styled.div`
-  /* width: 32rem; // NOTE: 설정하지 않으면 storybook에서 형태 무너짐
-  height: 27rem; */
-  border: 1px solid red;
+  width: 32rem;
   width: 66.6%;
   margin: 0 auto;
+`;
+
+export const Comment = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.4rem;
+`;
+
+export const ProgressBox = styled.div`
+  width: 100%;
+  border: 1.2px solid ${({ theme }) => theme.colors.gray[50]};
+  border-radius: 8px;
+  margin-bottom: 1.2rem;
+`;
+
+export const Rating = styled.div`
+  color: ${({ theme }) => theme.colors.black};
+  font-size: ${({ theme }) => theme.fontSizes[16]};
+  font-weight: 600;
+  width: 6.7rem;
+  line-height: 1.9rem;
+`;
+
+export const TotalReviews = styled.div`
+  color: ${({ theme }) => theme.colors.gray[200]};
+  font-size: ${({ theme }) => theme.fontSizes[12]};
+  font-weight: 500;
+  line-height: 1.4rem;
+`;
+
+export const OverallComment = styled.div`
+  color: ${({ theme }) => theme.colors.gray[200]};
+  font-size: ${({ theme }) => theme.fontSizes[14]};
+  font-weight: 500;
+  line-height: 1.7rem;
+  display: flex;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  margin-top: 0.8rem;
+  margin-bottom: 1.6rem;
+
+  span {
+    color: ${({ theme }) => theme.colors.primary};
+    margin-left: 0.4rem;
+  }
+`;
+
+export const Arrow = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 1.4rem;
+  font-size: ${({ theme }) => theme.fontSizes[12]};
+  color: ${({ theme }) => theme.colors.gray[200]};
+  margin-bottom: 1.2rem;
+  gap: 0.2rem;
+  font-weight: 500;
+  cursor: pointer;
+
+  svg {
+    stroke: ${({ theme }) => theme.colors.gray[200]};
+  }
 `;
--- components/OverallStats/OverallStats.tsx
+++ components/OverallStats/OverallStats.tsx
@@ -1,5 +1,82 @@
+import { useState } from "react";
+import { Theme, css } from "@emotion/react";
+import Heart from "assets/icons/heart.svg";
+import Pencil from "assets/icons/pencil.svg";
+import ArrowDown from "assets/icons/arrow-down.svg";
+import ArrowUp from "assets/icons/arrow-up.svg";
+import ProgressBar from "../ProgressBar/ProgressBar";
+import Button from "../shared/Button/Button";
 import * as S from "./OverallStats.styled";
 
-export default function OverallStats() {
-  return <S.Container>this is overallStats</S.Container>;
+interface IOverallStats {
+  rating: number;
+  totalReviews: number;
+  stats: {
+    optionName: string;
+    count: number;
+  }[];
+}
+
+// TODO: 상수 분리해야 함
+const SUBMIT_REVIEW = "리뷰쓰기";
+const SHOW_MORE = "더보기";
+const CLOSE = "접기";
+
+export default function OverallStats({ rating, totalReviews, stats }: IOverallStats) {
+  // TODO: 스토어의 당도는 휼륭해요 <- 이 데이터 없음
+  const [isOpened, setIsOpened] = useState(false);
+
+  const handleIsOpened = () => {
+    setIsOpened(() => !isOpened);
+  };
+
+  return (
+    <S.Container>
+      <S.Comment>
+        <Heart height={19} width={19} viewBox="0 0 17 17" fill="red" />
+        <S.Rating>당도 {rating}%</S.Rating>
+        <S.TotalReviews>({totalReviews}명 참여)</S.TotalReviews>
+      </S.Comment>
+      <S.OverallComment>
+        이 스토의 당도는 <span>훌륭해요!</span>
+      </S.OverallComment>
+      <S.ProgressBox>
+        <ProgressBar stats={stats} isOpened={isOpened} />
+        <S.Arrow onClick={handleIsOpened}>
+          {isOpened ? (
+            <>
+              {CLOSE}
+              <ArrowUp width={18} height={18} />
+            </>
+          ) : (
+            <>
+              {SHOW_MORE}
+              <ArrowDown width={18} height={18} />
+            </>
+          )}
+        </S.Arrow>
+      </S.ProgressBox>
+      <Button
+        type="submit"
+        label="submit review"
+        shape="square"
+        cssProp={({ colors, fontSizes }: Theme) =>
+          css`
+            width: 100%;
+            max-width: 48rem;
+            color: ${colors.primary};
+            border: 1px solid ${colors.primary};
+            font-size: ${fontSizes[14]};
+            gap: 0.4rem;
+          `
+        }
+      >
+        <>
+          {/** 색상 바꿔야 함 */}
+          <Pencil stroke="#F1424D" />
+          {SUBMIT_REVIEW}
+        </>
+      </Button>
+    </S.Container>
+  );
 }
