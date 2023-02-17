aeabbd64609aae0691c67e12b926a8e1598228de
rachel490 <ccori12@naver.com>
2023-02-17 18:35:07 +0900

    feat: Tab story 추가

---
+++ components/shared/Tab/Tab.stories.tsx
@@ -0,0 +1,29 @@
+import { css } from "@emotion/react";
+import { Meta, Story } from "@storybook/react";
+import { homeTab, storeTab } from "@/constants/navigations";
+import Tab, { IProp } from "./Tab";
+
+export default {
+  title: "shared/Tab",
+  component: Tab,
+  argTypes: {},
+} as Meta;
+
+const Template: Story<IProp> = args => <Tab {...args} />;
+
+export const SwipeableTab = Template.bind({});
+SwipeableTab.args = {
+  type: "swipeable",
+  menuList: storeTab,
+  cssProp: css``,
+};
+
+export const FixedTab = Template.bind({});
+FixedTab.args = {
+  type: "fixed",
+  menuList: homeTab,
+  cssProp: css`
+    --size: ${homeTab.length};
+    --padding: 1.6rem;
+  `,
+};
