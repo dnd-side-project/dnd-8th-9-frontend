2ddb0b02b8d6ec6feb8dccffe2e5770b1eb7a375
Serin-Kim <serin9864@gmail.com>
2023-02-17 20:32:20 +0900

    refactor: 당도별 문구 조건 수정

--- components/OverallStats/OverallStats.tsx
+++ components/OverallStats/OverallStats.tsx
@@ -23,11 +23,11 @@ const SHOW_MORE = "더보기";
 const CLOSE = "접기";
 
 const getOverallComment = (rating: number) => {
-  if (rating === 100) return "매우 높아요!";
-  if (rating >= 80) return "훌륭해요!";
-  if (rating >= 60) return "괜찮아요!";
-  if (rating >= 40) return "아쉬워요";
-  if (rating >= 20) return "별로예요";
+  if (rating >= 80) return "매우 높아요!";
+  if (rating >= 60) return "훌륭해요!";
+  if (rating >= 40) return "괜찮아요!";
+  if (rating >= 20) return "아쉬워요";
+  if (rating >= 0) return "별로예요";
   return "";
 };
 
