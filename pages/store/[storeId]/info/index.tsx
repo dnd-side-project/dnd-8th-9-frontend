37b34d53efd7074dc33db2d4cab8a656c680e4b7
Serin-Kim <serin9864@gmail.com>
2023-02-18 16:12:02 +0900

    feat: '정보' 탭(InfoPage) 구현
    Info 컴포넌트를 사용해 Info 페이지를 구현

--- pages/store/[storeId]/info/index.tsx
+++ pages/store/[storeId]/info/index.tsx
@@ -1,20 +1,15 @@
 import React from "react";
-import SellerInfo from "@/components/SellerInfo/SellerInfo";
-import Table from "@/components/Table/Table";
+import Info from "@/components/Info/Info";
+import { store } from "@/mocks/mockData/store";
 
-const sellerInfo = [
-  { key: "상호", value: "당도" },
-  { key: "주소", value: "서울시 마포구 마포대로 93번길 11 (망원동, 송학빌라) 1층" },
-  { key: "사업자등록번호", value: "000-01-012345" },
-  { key: "통신판매업번호", value: "2022-OOOO-1023" },
-  { key: "전화번호", value: "0507-1234-1234" },
-];
+const SELLER_INFO = "판매자 정보";
+const FOOD_INFO = "식품 정보";
 
 function InfoPage() {
   return (
     <>
-      <SellerInfo />
-      <Table option="info" data={sellerInfo} />
+      <Info title={SELLER_INFO} data={store.info.sellerInfo} />
+      <Info title={FOOD_INFO} data={store.info.foodInfo} />
     </>
   );
 }
