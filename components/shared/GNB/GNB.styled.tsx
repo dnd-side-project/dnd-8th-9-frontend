9ff4a737bbef36ea0be029bf249740160502d63b
Serin-Kim <serin9864@gmail.com>
2023-02-19 16:26:50 +0900

    feat: GNB 컴포넌트 구현

---
+++ components/shared/GNB/GNB.styled.tsx
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  max-width: 36rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 5.4rem;
+  margin: auto;
+  height: 7.4rem;
+  box-shadow: 0px -4px 12px rgba(80, 83, 105, 0.08);
+`;
