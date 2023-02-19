fb4bb9480ac00b9b30398aa609881373b2bd8c01
rachel490 <ccori12@naver.com>
2023-02-20 00:32:30 +0900

    style: 정보페이지 width 반응형으로 조정

--- pages/store/[storeId]/info/index.tsx
+++ pages/store/[storeId]/info/index.tsx
@@ -1,16 +1,24 @@
 import React from "react";
 import TableBox from "@/components/TableBox/TableBox";
 import { store } from "@/mocks/mockData/store";
+import StoreHero from "@/components/Store/StoreHero/StoreHero";
+import Tab from "@/components/shared/Tab/Tab";
+import { storeTab } from "@/constants/navigations";
+import * as S from "@/ui/store/[storeId]/info/info.styled";
 
 const SELLER_INFO = "판매자 정보";
 const FOOD_INFO = "식품 정보";
 
 function InfoPage() {
   return (
-    <>
-      <TableBox option="info" title={SELLER_INFO} data={store.info.sellerInfo} />
-      <TableBox option="info" title={FOOD_INFO} data={store.info.foodInfo} />
-    </>
+    <S.Container>
+      <StoreHero />
+      <Tab type="swipeable" menuList={storeTab} target="storeTab" />
+      <div>
+        <TableBox option="info" title={SELLER_INFO} data={store.info.sellerInfo} />
+        <TableBox option="info" title={FOOD_INFO} data={store.info.foodInfo} />
+      </div>
+    </S.Container>
   );
 }
 
