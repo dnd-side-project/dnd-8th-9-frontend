6782cf533d24369a21e56f5c780ec2f8ca667b94
rachel490 <ccori12@naver.com>
2023-02-19 22:37:20 +0900

    feat: menu card 클릭시 메뉴 상세로 이동 추가

--- pages/store/[storeId]/menu/index.tsx
+++ pages/store/[storeId]/menu/index.tsx
@@ -5,9 +5,12 @@ import StoreHero from "@/components/Store/StoreHero/StoreHero";
 import { storeTab } from "@/constants/navigations";
 import { menuList } from "@/mocks/mockData/menuList";
 import * as S from "@/ui/store/[storeId]/menu/menu.styled";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 function MenuPage() {
   const { data, isLoading, isError } = useGetStore({ storeId: 1 });
+  const { asPath } = useRouter();
 
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error...</div>;
@@ -18,7 +21,9 @@ function MenuPage() {
       <Tab type="swipeable" menuList={storeTab} />
       <S.ListWrap>
         {menuList.menus.map(menu => (
-          <Card menu={menu} key={menu.id} />
+          <Link key={menu.id} href={`${asPath}/${menu.id}`}>
+            <Card menu={menu} />
+          </Link>
         ))}
       </S.ListWrap>
     </S.Container>
