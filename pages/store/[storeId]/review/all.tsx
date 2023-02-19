94fc5431bee1a6fb265733a977e1d82aea8dc459
rachel490 <ccori12@naver.com>
2023-02-20 01:38:32 +0900

    feat: 리뷰 더보기 페이지 추가

---
+++ pages/store/[storeId]/review/all.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { reviews } from "@/mocks/mockData/review";
+import OverallStats from "@/components/OverallStats/OverallStats";
+import Review from "@/components/Review/Review";
+import * as S from "@/ui/store/[storeId]/review/review.styled";
+
+function all() {
+  const { rating, totalReviews, stats } = reviews.overallStats;
+
+  return (
+    <S.Container>
+      <OverallStats rating={rating} totalReviews={totalReviews} stats={stats} />
+      <S.ReviewListWrap>
+        {reviews.reviewList.map(review => (
+          <Review key={review.id} review={review} />
+        ))}
+      </S.ReviewListWrap>
+    </S.Container>
+  );
+}
+
+export default all;
