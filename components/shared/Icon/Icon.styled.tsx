70ab2801275648719fb8fbac7d941cc2d5b7e0c8
rachel490 <ccori12@naver.com>
2023-02-19 13:21:22 +0900

    feat: reusuable icon 컴포넌트 구현

---
+++ components/shared/Icon/Icon.styled.tsx
@@ -0,0 +1,51 @@
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+
+export interface IProp {
+  size: "xs" | "s" | "m" | "l";
+  color: string;
+  fill: string;
+}
+
+const SIZE_STYLE = {
+  xs: { width: "1.2rem", height: "1.2rem", "stroke-width": "1.2" },
+  s: { width: "1.6rem", height: "1.6rem", "stroke-width": "1.2" },
+  m: { width: "2rem", height: "2rem", "stroke-width": "1.6" },
+  l: { width: "2.4rem", height: "2.4rem", "stroke-width": "2" },
+};
+
+const sizeCss = ({ size }: IProp) => css`
+  svg {
+    width: ${SIZE_STYLE[size].width};
+    height: ${SIZE_STYLE[size].height};
+
+    path {
+      stroke-width: ${SIZE_STYLE[size]["stroke-width"]};
+    }
+  }
+`;
+
+const lineCss = ({ color }: IProp) => css`
+  svg path {
+    stroke: ${color};
+  }
+`;
+
+const fillCss = ({ fill }: IProp) => css`
+  svg {
+    fill: ${fill};
+
+    path {
+      stroke: ${fill};
+    }
+  }
+`;
+
+export const IconWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  ${fillCss};
+  ${lineCss};
+  ${sizeCss};
+`;
