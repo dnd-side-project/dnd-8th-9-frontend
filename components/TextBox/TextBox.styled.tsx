cc2f7d154934a2c603579135f385f573e7cc15b4
Serin-Kim <serin9864@gmail.com>
2023-02-19 14:36:54 +0900

    feat: TextBox 컴포넌트 추가

---
+++ components/TextBox/TextBox.styled.tsx
@@ -0,0 +1,27 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  padding: 1.6rem;
+  background-color: ${({ theme }) => theme.colors.background};
+  border-radius: 8px;
+  margin-bottom: 0.8rem;
+`;
+
+export const Name = styled.div`
+  color: ${({ theme }) => theme.colors.dark[500]};
+  font-size: ${({ theme }) => theme.fontSizes[14]};
+  font-weight: 600;
+  line-height: 1.7rem;
+  margin-bottom: 1.2rem;
+`;
+
+export const Value = styled.div`
+  font-size: ${({ theme }) => theme.fontSizes[13]};
+  font-weight: 400;
+  line-height: 2rem;
+  margin-bottom: 0.8rem;
+  ::before {
+    content: "•";
+    margin-right: 0.6rem;
+  }
+`;
