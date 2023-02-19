5d02e51b1ebd2a0b8f5faf2de15a0f84b2586454
rachel490 <ccori12@naver.com>
2023-02-19 23:51:30 +0900

    feat: review page에 업체상단 컴포넌트 & tab 렌더

--- pages/store/[storeId]/review/index.tsx
+++ pages/store/[storeId]/review/index.tsx
@@ -2,16 +2,24 @@ import React from "react";
 import { reviews } from "@/mocks/mockData/review"; // TODO: msw에서 받아와야 함
 import Review from "@/components/Review/Review";
 import OverallStats from "@/components/OverallStats/OverallStats";
+import StoreHero from "@/components/Store/StoreHero/StoreHero";
+import Tab from "@/components/shared/Tab/Tab";
+import { storeTab } from "@/constants/navigations";
+import * as S from "@/ui/store/[storeId]/review/review.styled";
 
 function ReviewPage() {
   const { rating, totalReviews, stats } = reviews.overallStats;
 
   return (
     <>
+      <StoreHero />
+      <Tab type="swipeable" menuList={storeTab} target="storeTab" />
       <OverallStats rating={rating} totalReviews={totalReviews} stats={stats} />
-      {reviews.reviewList.map(review => (
-        <Review key={review.id} review={review} />
-      ))}
+      <S.ReviewListWrap>
+        {reviews.reviewList.map(review => (
+          <Review key={review.id} review={review} />
+        ))}
+      </S.ReviewListWrap>
     </>
   );
 }
