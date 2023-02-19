55842cea4820ad36e704844eca31f2753776025e
rachel490 <ccori12@naver.com>
2023-02-19 13:20:44 +0900

    feat: icon constants file 추가

---
+++ components/shared/Icon/Icon.constant.tsx
@@ -0,0 +1,26 @@
+import * as i from "@/assets/icons/index";
+
+interface IIcon {
+  [key: string]: React.ReactNode;
+}
+export const ICONS: IIcon = {
+  pencil: <i.Pencil />,
+  arrowDown: <i.ArrowDown />,
+  arrowUp: <i.ArrowUp />,
+  heart: <i.Heart />,
+  delivery: <i.Delivery />,
+  ellipsis: <i.Ellipsis />,
+  info: <i.Info />,
+  instagram: <i.Instagram />,
+  leftArrow: <i.LeftArrow />,
+  like: <i.Like />,
+  map: <i.Map />,
+  clap: <i.Clap />,
+  speaker: <i.Speaker />,
+  thinking: <i.Thinking />,
+  warning: <i.Warning />,
+  store: <i.Store />,
+  bookmark: <i.Bookmark />,
+  share: <i.Share />,
+  tasty: <i.Tasty />,
+};
