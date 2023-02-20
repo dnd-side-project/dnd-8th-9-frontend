da8dbdda671b002a5115a3da7cefdcaca22ba0d5
rachel490 <ccori12@naver.com>
2023-02-20 20:59:12 +0900

    fix: store 대소문자 변경으로 인한 빌드 이슈 해결

--- pages/store/[storeId]/review/all.page.tsx
+++ pages/store/[storeId]/review/all.page.tsx
@@ -1,7 +1,6 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { reviews } from "@/mocks/mockData/review";
 import OverallStats from "@/components/store/review/OverallStats/OverallStats";
 import Review from "@/components/store/review/Review/Review";
+import { reviews } from "@/mocks/mockData/review";
 import * as S from "./review.styled";
 
 function ReviewsPage() {
