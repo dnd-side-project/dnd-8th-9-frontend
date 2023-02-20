f98a746eea86a72ecdd64b7b27bdd916d89e5ad1
rachel490 <ccori12@naver.com>
2023-02-20 18:09:24 +0900

    feat: 메뉴 상세 페이지에 리뷰 2개로 제한 및 리뷰 더보기 버튼 추가

--- pages/store/[storeId]/menu/[menuId]/index.tsx
+++ pages/store/[storeId]/menu/[menuId]/index.tsx
@@ -1,12 +1,32 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 import { menu } from "@/mocks/mockData/menu";
+import { reviews } from "@/mocks/mockData/review";
+import { menuList } from "@/mocks/mockData/menuList";
+
 import MenuHero from "@/components/Store/MenuHero/MenuHero";
 import Tab from "@/components/shared/Tab/Tab";
+import Button from "@/components/shared/Button/Button";
+import Review from "@/components/Review/Review";
+import Icon from "@/components/shared/Icon/Icon";
 import { storeMenuTab } from "@/constants/navigations";
+
 import { Caution, Size, Option, Flavor } from "@/assets/images/index";
 import * as S from "@/ui/store/[storeId]/menu/[menuId]/menuItem.styled";
 
 function MenuDetailsPage() {
+  const {
+    query: { menuId },
+    asPath,
+  } = useRouter();
+
   const { name, category, basePrice, menuImage, basicInfo } = menu;
+  const { reviewList } = reviews;
+  const targetMenu = menuList.menus.filter(menuItem => menuItem.id === Number(menuId));
+  const menuName = targetMenu.length && targetMenu[0].name;
+  const menuReviews = reviewList.filter(review => review.menuOption === menuName);
+
   return (
     <S.Container>
       <MenuHero
@@ -21,6 +41,22 @@ function MenuDetailsPage() {
       <Flavor />
       <Option />
       <Caution />
+      <S.ReviewListWrap>
+        <h2>
+          이 메뉴의 리뷰<strong>{menuReviews.length}</strong>
+        </h2>
+        {menuReviews?.slice(0, 2).map(review => (
+          <Review key={review.id} review={review} />
+        ))}
+        <Link href={`${asPath}/all`}>
+          <Button type="button" label="more reviews" shape="square" cssProp={S.buttonCss}>
+            <>
+              리뷰 전체보기
+              <Icon name="arrowRight" size="m" />
+            </>
+          </Button>
+        </Link>
+      </S.ReviewListWrap>
     </S.Container>
   );
 }
