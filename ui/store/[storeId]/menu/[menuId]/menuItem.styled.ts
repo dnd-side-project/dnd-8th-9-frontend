1abc175a3d542340263dfbbfff886c8626b9dfd1
rachel490 <ccori12@naver.com>
2023-02-20 18:10:02 +0900

    style: 메뉴 상세 페이지 스타일링 추가

--- ui/store/[storeId]/menu/[menuId]/menuItem.styled.ts
+++ ui/store/[storeId]/menu/[menuId]/menuItem.styled.ts
@@ -1,5 +1,34 @@
+import { css, Theme } from "@emotion/react";
 import styled from "@emotion/styled";
 
 export const Container = styled.div`
-  border: 1px solid black;
+  padding-bottom: 4.5rem;
+`;
+
+export const ReviewListWrap = styled.div`
+  border-top: 0.7rem solid ${({ theme }) => theme.colors.gray[70]};
+  width: 100%;
+  padding: 3.2rem 1.6rem 2rem;
+
+  h2 {
+    font-size: ${({ theme }) => theme.fontSizes[16]};
+    font-weight: 600;
+    color: ${({ theme }) => theme.colors.gray[900]};
+
+    strong {
+      color: ${({ theme }) => theme.colors.primary};
+      font-weight: 700;
+      margin-left: 0.4rem;
+    }
+  }
+`;
+
+export const buttonCss = ({ colors, fontSizes }: Theme) => css`
+  border-radius: 0.8rem;
+  border: 1px solid ${colors.gray[200]};
+  width: 100%;
+  margin-bottom: 3rem;
+  font-size: ${fontSizes[14]};
+  color: ${colors.gray[700]};
+  font-weight: 500;
 `;
