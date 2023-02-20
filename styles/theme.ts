62cb7ae0a323f6ff8abb46c8d3806c52813b2f4f
rachel490 <ccori12@naver.com>
2023-02-20 18:58:54 +0900

    style: theme color 재정리

--- styles/theme.ts
+++ styles/theme.ts
@@ -21,27 +21,28 @@ const theme: Theme = {
   colors: {
     primary: "#F1424D",
     secondary: "#FFA4A9",
-    dark: {
-      100: "#7C87AD",
+    navy: {
+      100: "#F7F8FA",
+      200: "#7C87AD",
       300: "#4C536D",
-      500: "#0E1B47",
+      400: "#0E1B47",
     },
     gray: {
-      50: "#F0F0F0",
-      70: "#F2F2F2",
-      80: "#E9E9E9",
-      90: "#EEEEEE",
-      95: "#EFEFEF",
       100: "#D7D7D7",
       200: "#A2A2A2",
       300: "#A5A5A5",
+      400: "#525252",
       500: "#3C3C3C",
-      700: "#525252",
-      900: "#202020",
     },
-    background: "#F7F8FA",
-    black: "#000",
-    white: "#fff",
+    white: {
+      100: "#fff",
+      200: "#F2F2F2",
+      300: "#F0F0F0",
+      400: "#EFEFEF",
+      500: "#EEEEEE",
+      600: "#E9E9E9",
+    },
+    black: "#202020",
   },
 };
 
