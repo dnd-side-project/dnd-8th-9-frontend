56ed864ea74417a2895a1a95c0806ba8e0b2a0b9
Serin-Kim <serin9864@gmail.com>
2023-02-23 21:40:11 +0900

    feat: 업체상세 / 리뷰 / 사진 모아보기 페이지 구현

---
+++ pages/store/[storeId]/review/all/images.page.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/no-array-index-key */
+import Image from "next/image";
+import { reviews } from "@/mocks/mockData/review"; // TODO: 나중에 바꿔야 함
+import ImageWrap from "@/components/shared/ImageWrap/ImageWrap";
+import CountTitle from "@/components/store/review/CountTitle/CountTitle";
+import * as S from "./images.styled";
+
+const IMAGES = "이미지";
+
+export default function ImagesPage() {
+  const reviewImages = reviews.reviewList.map(review => review.reviewImages).flat();
+
+  return (
+    <S.Container>
+      <CountTitle tag={IMAGES} count={reviewImages.length} />
+      <S.Images>
+        {reviewImages.map((image, idx) => (
+          // eslint-disable-next-line react/jsx-key
+          <ImageWrap key={idx} percent={32}>
+            <Image src={image.url} width={108} height={106} alt={IMAGES} />
+          </ImageWrap>
+        ))}
+      </S.Images>
+    </S.Container>
+  );
+}
---
+++ pages/store/[storeId]/review/all/images.styled.tsx
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  width: 100%;
+  padding: 3.2rem 1.6rem 2rem;
+`;
+
+export const Images = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 4px;
+  margin-top: 2.4rem;
+`;
