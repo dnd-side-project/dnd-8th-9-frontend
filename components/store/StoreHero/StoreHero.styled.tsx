b2ea7eb2186009ccf26b9363f24716b1c565d769
rachel490 <ccori12@naver.com>
2023-02-24 05:03:17 +0900

    style: storeHero의 mainInfo border-radius 추가

--- components/store/StoreHero/StoreHero.styled.tsx
+++ components/store/StoreHero/StoreHero.styled.tsx
@@ -3,11 +3,15 @@ import styled from "@emotion/styled";
 export const Container = styled.div`
   position: relative;
   width: 100%;
+  padding-top: 47rem;
 `;
 
 export const CarouselWrap = styled.div`
   width: 100%;
   height: 50rem;
+  position: absolute;
+  top: 0;
+  z-index: -99;
 `;
 
 export const Section = styled.section`
@@ -18,10 +22,7 @@ export const Section = styled.section`
 
 export const MainInfoSection = styled(Section)`
   padding: 3rem 1.6rem 1.4rem;
-  /* border-top-left-radius: 2.4rem;
-  border-top-right-radius: 2.4rem;
-  position: absolute;
-  width: 100%;
-  bottom: -290px;
-  z-index: 1; */
+  background-color: ${({ theme }) => theme.colors.white[100]};
+  border-top-left-radius: 1.6rem;
+  border-top-right-radius: 1.6rem;
 `;
