1e6abb2cc957cec287aabc18973b2a60a339308b
rachel490 <ccori12@naver.com>
2023-02-24 06:30:35 +0900

    style: 메뉴 사이즈 원 svg 파일 추가

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -2,3 +2,4 @@ export { default as Caution } from "./caution.svg";
 export { default as Size } from "./size.svg";
 export { default as Option } from "./option.svg";
 export { default as Flavor } from "./flavor.svg";
+export { default as SizeCircle } from "./sizeCircle.svg";
