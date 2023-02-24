69e3d5f597e762dc635cdef4847cb3c12bfbd2a7
rachel490 <ccori12@naver.com>
2023-02-25 06:01:32 +0900

    feat: 페이지별 navbar, gnb 데이터 객체로 정리

--- constants/navigations.ts
+++ constants/navigations.ts
@@ -1,110 +1,85 @@
-export const storeTab = [
-  {
-    label: "메뉴",
-    link: "/menu",
-  },
-  {
-    label: "주문양식",
-    link: "/order",
-  },
-  {
-    label: "리뷰",
-    link: "/review",
-  },
-  {
-    label: "공지사항",
-    link: "/notice",
-  },
-  {
-    label: "정보",
-    link: "/info",
-  },
-];
+import theme from "@/styles/theme";
+import { ROUTES } from "./routes";
 
-export const storeMenuTab = [
-  {
-    label: "사이즈",
-    name: "size",
-    link: "#size",
-  },
-  {
-    label: "맛",
-    link: "#flavor",
-  },
-  {
-    label: "모양 변경",
-    link: "#variation",
-  },
-  {
-    label: "주의사항",
-    link: "#caution",
-  },
-  {
-    label: "리뷰",
-    link: "#review",
-  },
-];
+interface INavigationItem {
+  title: string;
+  back?: boolean;
+  icon?: boolean;
+  bgColor?: string;
+  scrollBgColor?: string;
+  iconColor?: string;
+}
 
-export const resultTab = [
-  {
-    label: "메뉴",
-    link: "/menu",
-  },
-  {
-    label: "스토어",
-    link: "/store",
-  },
-];
+interface INavigationHeader {
+  [key: string]: INavigationItem;
+}
 
-export const homeTab = [
-  {
-    label: "당도추천",
-    link: "/dango",
+export const NAVIGATION_HEADER: INavigationHeader = {
+  [ROUTES.STORE_MENU_LIST_PAGE]: {
+    title: "업체명",
+    bgColor: "transparent",
+    scrollBgColor: theme.colors.white[100],
   },
-  {
-    label: "스토어",
-    link: "/store",
+  [ROUTES.STORE_MENU_ITEM_PAGE]: {
+    title: "업체명",
+    icon: false,
   },
-  {
-    label: "리얼리뷰",
-    link: "/review",
+  [ROUTES.STORE_MENU_ITEM_REVIEWS_PAGE]: {
+    title: "리뷰",
+    icon: false,
   },
-];
+  [ROUTES.STORE_REVIEW_PAGE]: {
+    title: "업체명",
+    bgColor: "transparent",
+    scrollBgColor: "white",
+  },
+  [ROUTES.STORE_REVIEW_IMAGE_PAGE]: {
+    title: "",
+    icon: false,
+    bgColor: "black",
+    iconColor: "white",
+  },
+  [ROUTES.STORE_REVIEWS_PAGE]: {
+    title: "리뷰",
+    icon: false,
+  },
+  [ROUTES.STORE_IMAGE_REVIEWS_PAGE]: {
+    title: "리뷰",
+    icon: false,
+  },
+  [ROUTES.STORE_IMAGE_REVIEW_ITEM_PAGE]: {
+    title: "",
+    icon: false,
+    bgColor: "black",
+    iconColor: "white",
+  },
+  [ROUTES.STORE_ORDER_FORM_PAGE]: {
+    title: "업체명",
+    bgColor: "transparent",
+    scrollBgColor: "white",
+  },
+  [ROUTES.STORE_NOTICES_PAGE]: {
+    title: "업체명",
+    bgColor: "transparent",
+    scrollBgColor: "white",
+  },
+  [ROUTES.STORE_INFO_PAGE]: {
+    title: "업체명",
+    bgColor: "transparent",
+    scrollBgColor: "white",
+  },
+};
 
-export const categoryTab = [
-  {
-    label: "레터링",
-    link: "#",
-  },
-  {
-    label: "캐릭터/입체",
-    link: "#",
-  },
-  {
-    label: "포토",
-    link: "#",
-  },
-  {
-    label: "꽃",
-    link: "#",
-  },
-];
-
-export const navTab = [
-  {
-    label: "홈",
-    link: "/",
-  },
-  {
-    label: "검색",
-    link: "/search",
-  },
-  {
-    label: "북마크",
-    link: "/bookmark",
-  },
-  {
-    label: "마이페이지",
-    link: "/mypage",
-  },
-];
+export const GNB = {
+  [ROUTES.STORE_MENU_LIST_PAGE]: true,
+  [ROUTES.STORE_MENU_ITEM_PAGE]: true,
+  [ROUTES.STORE_MENU_ITEM_REVIEWS_PAGE]: true,
+  [ROUTES.STORE_REVIEW_PAGE]: true,
+  [ROUTES.STORE_REVIEW_IMAGE_PAGE]: false,
+  [ROUTES.STORE_REVIEWS_PAGE]: true,
+  [ROUTES.STORE_IMAGE_REVIEWS_PAGE]: false,
+  [ROUTES.STORE_IMAGE_REVIEW_ITEM_PAGE]: false,
+  [ROUTES.STORE_ORDER_FORM_PAGE]: true,
+  [ROUTES.STORE_NOTICES_PAGE]: true,
+  [ROUTES.STORE_INFO_PAGE]: true,
+};
