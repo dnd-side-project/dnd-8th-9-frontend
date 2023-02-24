c1d3277c249b975e9dac40994a9a1642309f68e9
rachel490 <ccori12@naver.com>
2023-02-25 06:23:19 +0900

    feat: bookmark, share 버튼에 이벤트 추가

--- components/shared/Navbar/Navbar.tsx
+++ components/shared/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
 import { useTheme } from "@emotion/react";
 import { NAVIGATION_HEADER } from "@/constants/navigations";
@@ -8,10 +8,9 @@ import * as S from "./Navbar.styled";
 function Navbar() {
   // NOTE: iconColor, Title, backIcon, moreIcon
 
-  // TODO: 뒤로가기 기능 추가 / 버튼화
-  // TODO: 북마크 클릭시 색깔 변화 / 버튼화 / 이벤트 추가 (콘솔)
-  // TODO: 공유 클릭 버튼화 / 이벤트 추가 (콘솔)
   // TODO: 스크롤 이벤트 추가 (텍스트 없이 아이콘 투명 -> 하얀색 배경 + 텍스트 & 아이콘)
+
+  const [isBookmarked, setIsBookmarked] = useState(false);
 
   const {
     back: previous,
@@ -33,6 +32,15 @@ function Navbar() {
     previous();
   };
 
+  const bookmark = () => {
+    setIsBookmarked(prev => !prev);
+    console.log("bookmarked");
+  };
+
+  const share = () => {
+    console.log("share");
+  };
+
   return (
     <S.NavbarWrap bgColor={bgColor}>
       <button
@@ -47,10 +55,15 @@ function Navbar() {
       <div className="etcButtons">
         {icon && (
           <>
-            <button aria-label="bookmark" type="button">
-              <Icon name="saveBookmark" size="l" color={iconColor} />
+            <button aria-label="bookmark" type="button" onClick={bookmark}>
+              <Icon
+                name="saveBookmark"
+                size="l"
+                color={isBookmarked ? colors.primary : iconColor}
+                fill={isBookmarked ? colors.primary : "transparent"}
+              />
             </button>
-            <button aria-label="share" type="button">
+            <button aria-label="share" type="button" onClick={share}>
               <Icon name="share" size="l" color={iconColor} fill={iconColor} />
             </button>
           </>
