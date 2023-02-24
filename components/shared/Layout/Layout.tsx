36402f0d68c7ee2fe5cd19fb57491e6e7343684b
rachel490 <ccori12@naver.com>
2023-02-25 07:39:57 +0900

    feat: GNB path에 따라서 조건부 렌더링 추가

--- components/shared/Layout/Layout.tsx
+++ components/shared/Layout/Layout.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { NAVIGATION_HEADER } from "@/constants/navigations";
+import { GNB_VISIBILITY, NAVIGATION_HEADER } from "@/constants/navigations";
 import GNB from "../GNB/GNB";
 import Navbar from "../Navbar/Navbar";
 import * as S from "./Layout.styled";
@@ -9,16 +9,22 @@ interface IChildren {
 }
 
 const AppLayout = ({ children }: IChildren) => {
+  // TODO:
+  // 1. GNB 조건부 렌더링
+  // 2. GNB 클릭시 색깔 변경
+  // 3. ROUTE -> nav, gnb 나머지 모두 추가 (draft)
+
   const { pathname } = useRouter();
 
   const { scrollBgColor = "" } = NAVIGATION_HEADER[pathname];
+  const haveGNB = GNB_VISIBILITY[pathname];
 
   return (
     <S.Centering>
       <S.FixedWidth>
         <Navbar />
         <S.Main noPaddingTop={!!scrollBgColor}>{children}</S.Main>
-        <GNB />
+        {haveGNB && <GNB />}
       </S.FixedWidth>
     </S.Centering>
   );
