fcdbb2f462d2999a8660e6a0d07c9f3ef9e8f7e3
rachel490 <ccori12@naver.com>
2023-03-01 20:32:10 +0900

    style: 변경된 디자인의 color theme 추가

--- styles/emotion.d.ts
+++ styles/emotion.d.ts
@@ -45,6 +45,37 @@ declare module "@emotion/react" {
         600: string;
       };
       black: string;
+      pink: {
+        100: string;
+        200: string;
+        300: string;
+        400: string;
+        500: string;
+        600: string;
+        700: string;
+        800: string;
+      };
+      blue: {
+        100: string;
+        200: string;
+        300: string;
+        400: string;
+        500: string;
+        600: string;
+        700: string;
+        800: string;
+      };
+      grey: {
+        100: string;
+        200: string;
+        300: string;
+        400: string;
+        500: string;
+        600: string;
+        700: string;
+        800: string;
+        900: string;
+      };
     };
   }
 }
