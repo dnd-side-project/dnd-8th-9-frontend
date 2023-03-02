a2b183cdb3288a4b399443c642482a31534b9c2d
rachel490 <ccori12@naver.com>
2023-03-02 10:09:07 +0900

    feat: 홈피드에 Home Hero 렌더

--- pages/home/recommendation.page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function HomeRecommendationPage() {
-  return <div>HomeRecommendationPage</div>;
-}
-
-export default HomeRecommendationPage;
---
+++ pages/home/recommendation/index.page.tsx
@@ -0,0 +1,11 @@
+import HomeHero from "@/components/home/HomeHero/HomeHero";
+
+function HomeRecommendationPage() {
+  return (
+    <>
+      <HomeHero />
+    </>
+  );
+}
+
+export default HomeRecommendationPage;
