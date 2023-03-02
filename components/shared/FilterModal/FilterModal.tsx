9d0f4c917b290244019f3c4f15e2431143444fe1
rachel490 <ccori12@naver.com>
2023-03-02 12:43:34 +0900

    feat: filter modal에 filter option들 추가

--- components/shared/FilterModal/FilterModal.tsx
+++ components/shared/FilterModal/FilterModal.tsx
@@ -1,6 +1,8 @@
 import Sheet from "react-modal-sheet";
 import { css, useTheme } from "@emotion/react";
 import { filterTab } from "@/constants/tabs";
+import CheckboxGroup from "../Input/Checkbox/CheckboxGroup";
+import RadioGroup from "../Input/Radio/RadioGroup";
 import Text from "../Text/Text";
 import * as S from "./FilterModal.styled";
 
@@ -8,6 +10,31 @@ interface IProp {
   isOpen: boolean;
   toggleModal: () => void;
 }
+
+interface IFilterTabMenu {
+  [key: string]: {
+    type: "checkbox" | "radio";
+    options: string[];
+  };
+}
+
+const FILTER_TAB_MENU: IFilterTabMenu = {
+  카테고리: {
+    type: "checkbox",
+    options: ["레터링", "캐릭터/입체", "포토", "꽃", "기타"],
+  },
+  가격: {
+    type: "radio",
+    options: ["1만원 ~ 2만원", "2만원 ~ 3만원", "3만원 ~ 4만원", "4만원 이상"],
+  },
+  주문플랫폼: {
+    type: "checkbox",
+    options: ["인스타그램", "아이디어스", "네이버스토어", "카카오톡 채널", "업체 홈페이지"],
+  },
+  수령방법: { type: "radio", options: ["메장에서 픽업", "택배로 배송"] },
+};
+
+const currentTab = "가격";
 
 function FilterModal({ isOpen, toggleModal }: IProp) {
   const { colors } = useTheme();
@@ -29,6 +56,11 @@ function FilterModal({ isOpen, toggleModal }: IProp) {
               --size: ${filterTab.length};
             `}
           />
+          {FILTER_TAB_MENU[currentTab].type === "checkbox" ? (
+            <CheckboxGroup options={FILTER_TAB_MENU[currentTab].options} />
+          ) : (
+            <RadioGroup options={FILTER_TAB_MENU[currentTab].options} />
+          )}
         </Sheet.Content>
       </Sheet.Container>
       <Sheet.Backdrop />
