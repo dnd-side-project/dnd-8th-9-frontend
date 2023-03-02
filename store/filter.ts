f257b66bfe08c54d3ed3376cd58214e537d5f308
rachel490 <ccori12@naver.com>
2023-03-02 15:08:24 +0900

    feat: currentFilterTab state 추가 및 change action 추가

--- store/filter.ts
+++ store/filter.ts
@@ -1,16 +1,22 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
 
+type TFilterTab = "카테고리" | "가격" | "주문플랫폼" | "수령방법";
+
 interface FilterState {
+  currentFilterTab: TFilterTab;
   filterModalOpen: boolean;
   toggleFilterModalOpen: () => void;
+  changeCurrentFilterTab: (newFilterTab: TFilterTab) => void;
 }
 
 const useFilterStore = create<FilterState>()(
   devtools(set => ({
+    currentFilterTab: "카테고리",
     filterModalOpen: false,
 
     toggleFilterModalOpen: () => set(state => ({ filterModalOpen: !state.filterModalOpen })),
+    changeCurrentFilterTab: (newFilterTab: TFilterTab) => set({ currentFilterTab: newFilterTab }),
   })),
 );
 
