aaec74d842360b5afa06ca88c7e8ff8ce0df7f77
rachel490 <ccori12@naver.com>
2023-03-03 00:39:49 +0900

    style: 당도 텍스트 로고 svg 이미지 추가

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -1,3 +1,4 @@
 export { default as SizeCircle } from "./sizeCircle.svg";
 export { default as UploadImage } from "./uploadImage.svg";
 export { default as Cake } from "./cake.svg";
+export { default as DangdoLogo } from "./dangdoLogo.svg";
