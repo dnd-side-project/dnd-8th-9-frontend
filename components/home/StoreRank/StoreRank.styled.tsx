2a9c7daaaa0ad7e728e0ec3bb5c20c1d9913a999
rachel490 <ccori12@naver.com>
2023-03-02 10:32:20 +0900

    feat: Store Rank 컴포넌트 구현 (carousel)

---
+++ components/home/StoreRank/StoreRank.styled.tsx
@@ -0,0 +1,40 @@
+import Button from "@/components/shared/Button/Button";
+import { css, Theme } from "@emotion/react";
+import styled from "@emotion/styled";
+
+export const ButtonList = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 1rem;
+  margin: 1.2rem 0 2.4rem;
+`;
+
+export const CategoryButton = styled(Button)`
+  padding: 0.9rem 1.4rem;
+  border: 1px solid ${({ theme }) => theme.colors.grey[300]};
+
+  &.isSelected {
+    background-color: ${({ theme }) => theme.colors.blue[800]};
+    border-color: ${({ theme }) => theme.colors.blue[800]};
+
+    span {
+      ${({ theme }) => theme.colors.blue[800]};
+      color: ${({ theme }) => theme.colors.grey[100]};
+    }
+  }
+`;
+
+export const CardWrap = styled.div`
+  margin-bottom: 1.6rem;
+`;
+
+export const SwiperWrap = styled.div`
+  .swiper {
+    .swiper-wrapper {
+      margin-bottom: 3rem;
+    }
+    .swiper-pagination-bullet {
+      background-color: ${({ theme }) => theme.colors.grey[900]};
+    }
+  }
+`;
---
+++ components/home/StoreRank/StoreRank.tsx
@@ -0,0 +1,63 @@
+/* eslint-disable react/no-array-index-key */
+import React, { useState } from "react";
+import { useTheme } from "@emotion/react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+
+import { storeList } from "@/mocks/mockData/storeList";
+import { TCategory } from "@/api/types/shared";
+import { chunkArray } from "@/utils/util";
+
+import Text from "@/components/shared/Text/Text";
+import StoreSingleCard from "@/components/shared/Card/StoreSingleCard";
+import * as S from "./StoreRank.styled";
+
+const CATEGORY_LIST = ["레터링", "캐릭터/입체", "포토", "꽃"];
+
+function StoreRank() {
+  const { colors } = useTheme();
+  const [currentCategory, setCurrentCategory] = useState<TCategory>("레터링");
+
+  const changeCategory = (category: TCategory) => {
+    setCurrentCategory(category);
+  };
+
+  return (
+    <div>
+      <Text size={18} weight={600} color={colors.grey[900]}>
+        마포구 스타일별 BEST 스토어
+      </Text>
+      <S.ButtonList>
+        {CATEGORY_LIST.map(category => (
+          <S.CategoryButton
+            key={category}
+            onClick={() => changeCategory(category as TCategory)}
+            className={currentCategory === category ? "isSelected" : ""}
+            label={category}
+            type="button"
+            shape="round"
+          >
+            <Text size={15} color={colors.grey[800]}>
+              {category}
+            </Text>
+          </S.CategoryButton>
+        ))}
+      </S.ButtonList>
+      <S.SwiperWrap>
+        <Swiper modules={[Pagination]} pagination={{ clickable: true }} slidesPerView={1}>
+          {chunkArray(storeList, 3).map((chunk, idx) => (
+            <SwiperSlide key={idx} style={{ textAlign: "left" }}>
+              {chunk.map((store, index) => (
+                <S.CardWrap key={store.id}>
+                  <StoreSingleCard data={store} rank={3 * idx + 1 + index} />
+                </S.CardWrap>
+              ))}
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </S.SwiperWrap>
+    </div>
+  );
+}
+
+export default StoreRank;
