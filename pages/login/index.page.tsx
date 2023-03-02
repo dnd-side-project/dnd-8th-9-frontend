89fc0682157eb8578c46be172b8df3f6c72d0da5
rachel490 <ccori12@naver.com>
2023-03-03 00:40:45 +0900

    feat: 로그인 화면 구현

--- pages/login/index.page.tsx
+++ pages/login/index.page.tsx
@@ -1,57 +1,31 @@
-/* eslint-disable no-alert */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/naming-convention */
-import KakaoLogin from "react-kakao-login";
-import Icon from "@/components/shared/Icon/Icon";
-import { Text } from "@/components/shared/Text/Text.styled";
-import useUserStore from "@/store/user";
+import Image from "next/image";
+import { useTheme } from "@emotion/react";
+import { DangdoLogo } from "@/assets/images";
+import KakaoLogin from "@/components/login/KakaoLogin";
+import Text from "@/components/shared/Text/Text";
+import * as S from "./login.styled";
 
 function LoginPage() {
-  const { loginUser, logoutUser } = useUserStore();
-
-  const loginSuccessHandler = (res: any) => {
-    const {
-      response: { access_token },
-      profile: {
-        properties: { nickname, profile_image },
-        kakao_account: { email },
-      },
-    } = res;
-
-    localStorage.setItem("access_token", access_token as string);
-    loginUser(nickname, profile_image, email);
-  };
-
-  const logoutHandler = () => {
-    localStorage.removeItem("access_token");
-    logoutUser();
-  };
+  const { colors } = useTheme();
 
   return (
-    <KakaoLogin
-      token={process.env.NEXT_PUBLIC_KAKAO_LOGIN_API_KEY as string}
-      onSuccess={loginSuccessHandler}
-      onFail={error => alert(error)}
-      onLogout={logoutHandler}
-      style={{
-        backgroundColor: "#FEE500",
-        borderRadius: "0.8rem",
-        height: "4.8rem",
-        width: "100%",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        columnGap: "1rem",
-      }}
-    >
-      <>
-        <Icon name="kakao" color="black" fill="black" />
-        <Text weight={500} size={16} color="black">
-          카카오로 시작하기
+    <S.LoginWrap>
+      <S.LogoWrap>
+        <DangdoLogo />
+        <Text size={14} weight={500} color={colors.grey[100]}>
+          주문제작 케이크 탐색을 쉽고 빠르게
         </Text>
-      </>
-    </KakaoLogin>
+      </S.LogoWrap>
+      <S.ImageWrap>
+        <Image src="/loginCake.svg" alt="3d cake" fill />
+      </S.ImageWrap>
+      <S.ButtonWrap>
+        <KakaoLogin />
+        <S.LaterButton label="login later" type="button" shape="square">
+          로그인 없이 둘러보기
+        </S.LaterButton>
+      </S.ButtonWrap>
+    </S.LoginWrap>
   );
 }
 
