e5e133d1b702e02d1284527c874ca6c8845721b1
rachel490 <ccori12@naver.com>
2023-03-02 12:46:40 +0900

    feat: 홈피드 스토어 탭에 filterModal 추가

--- pages/home/stores.page.tsx
+++ pages/home/stores.page.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { css, useTheme } from "@emotion/react";
 
 import { storeList } from "@/mocks/mockData/storeList";
@@ -6,11 +7,17 @@ import { homeTab } from "@/constants/tabs";
 import HomeHero from "@/components/home/HomeHero/HomeHero";
 import Tab from "@/components/shared/Tab/Tab";
 import Sort from "@/components/shared/Sort/Sort";
+import FilterModal from "@/components/shared/FilterModal/FilterModal";
 import StoreDoubleCard from "@/components/shared/Card/StoreDoubleCard";
 import * as S from "./recommendation/recommendation.styled";
 
 function HomeStoresPage() {
   const { colors } = useTheme();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsOpen(prev => !prev);
+  };
 
   return (
     <div>
@@ -26,11 +33,13 @@ function HomeStoresPage() {
         `}
       />
       <S.ContentWrap>
+        <button onClick={toggleModal}>필터링</button>
         <Sort />
         {storeList.map(store => (
           <StoreDoubleCard key={store.id} data={store} />
         ))}
       </S.ContentWrap>
+      <FilterModal isOpen={isOpen} toggleModal={toggleModal} />
     </div>
   );
 }
