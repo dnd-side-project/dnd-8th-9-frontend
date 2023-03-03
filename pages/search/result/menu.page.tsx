6754349393cc7edd28b14c05b5d94fffcf8fc881
rachel490 <ccori12@naver.com>
2023-03-04 03:25:48 +0900

    feat: 메뉴 검색 구현

--- pages/search/result/menu.page.tsx
+++ pages/search/result/menu.page.tsx
@@ -1,6 +1,10 @@
+import { useEffect, useState } from "react";
 import { css, useTheme } from "@emotion/react";
 import { resultTab } from "@/constants/tabs";
+import { IRandomMenuItem } from "@/api/types/randomMenuList";
 import { randomMenus } from "@/mocks/mockData/randomMenuList";
+import useSearchStore from "@/store/search";
+import useFilterStore from "@/store/filter";
 
 import SearchNav from "@/components/search/SearchNav/SearchNav";
 import FilterBar from "@/components/shared/FilterBar/FilterBar";
@@ -12,6 +16,28 @@ import * as S from "../search.styled";
 
 function SearchResultMenuPage() {
   const { colors } = useTheme();
+  const { currentSearch } = useSearchStore();
+  const { selectedFilterOptions } = useFilterStore();
+  const [searchResult, setSearchResult] = useState<IRandomMenuItem[]>([]);
+  const [filterResult, setFilterResult] = useState<IRandomMenuItem[]>([]);
+  const [data, setData] = useState<IRandomMenuItem[]>(searchResult || filterResult || []);
+
+  useEffect(() => {
+    const result = randomMenus.menus.filter(menu => menu.name.includes(currentSearch));
+    setSearchResult(result);
+    setData(result);
+  }, [currentSearch]);
+
+  useEffect(() => {
+    if (
+      Object.keys(selectedFilterOptions).some(filter => selectedFilterOptions[filter].length !== 0)
+    ) {
+      const result = searchResult.filter(menu => menu.price > 40000);
+      setFilterResult(result);
+      setData(result);
+    }
+  }, [searchResult, selectedFilterOptions]);
+
   return (
     <S.Layout>
       <SearchNav mode="result" />
@@ -28,11 +54,11 @@ function SearchResultMenuPage() {
       />
       <Sort />
       <FilterBar />
-      <S.Grid>
-        {randomMenus.menus.map(menu => (
+      <S.MenuGrid>
+        {data.map(menu => (
           <MenuDoubleCard key={menu.id} data={menu} />
         ))}
-      </S.Grid>
+      </S.MenuGrid>
       <FilterModal />
     </S.Layout>
   );
