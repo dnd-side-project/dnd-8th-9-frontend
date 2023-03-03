0581dbc159793a47c847b8d7eacc51eed0f57e15
Serin-Kim <serin9864@gmail.com>
2023-03-04 00:34:18 +0900

    feat: 북마크 해제 기능 구현

--- store/bookmark.ts
+++ store/bookmark.ts
@@ -23,14 +23,23 @@ const useBookmarkStore = create<BookmarkState>()(
       bookmarkMenuList: [],
       updateBookmarkStoreList: (newStore: IStoreItem) =>
         set(state => {
-          if (state.bookmarkStoreList.findIndex(store => store.id === newStore.id) === -1) {
+          const isNewStore =
+            state.bookmarkStoreList.findIndex(store => store.id === newStore.id) === -1;
+          if (isNewStore) {
             state.bookmarkStoreList.push(newStore);
+          } else {
+            const index = state.bookmarkStoreList.findIndex(store => store.id === newStore.id);
+            state.bookmarkStoreList.splice(index, 1);
           }
         }),
       updateBookmarkMenuList: (newMenu: IRandomMenuItem) =>
         set(state => {
-          if (state.bookmarkMenuList.findIndex(menu => menu.id === newMenu.id) === -1) {
+          const isNewMenu = state.bookmarkMenuList.findIndex(menu => menu.id === newMenu.id) === -1;
+          if (isNewMenu) {
             state.bookmarkMenuList.push(newMenu);
+          } else {
+            const index = state.bookmarkMenuList.findIndex(menu => menu.id === newMenu.id);
+            state.bookmarkMenuList.splice(index, 1);
           }
         }),
     })),
