97bcec92398925f08892a676b33566466bf42dcf
rachel490 <ccori12@naver.com>
2023-03-03 11:20:47 +0900

    feat: logout modal의 버튼에 로그아웃 및 취소 로직 추가

--- components/mypage/LogoutModal/LogoutModal.tsx
+++ components/mypage/LogoutModal/LogoutModal.tsx
@@ -1,12 +1,23 @@
+import { useRouter } from "next/router";
 import Sheet from "react-modal-sheet";
 import { useTheme } from "@emotion/react";
+import { ROUTES } from "@/constants/routes";
 import useModalStore from "@/store/modal";
+import useUserStore from "@/store/user";
 import Text from "@/components/shared/Text/Text";
 import * as S from "./LogoutModal.styled";
 
 function LogoutModal() {
   const { colors } = useTheme();
+  const { push } = useRouter();
   const { logoutModalOpen, toggleLogoutModal } = useModalStore();
+  const { logoutUser } = useUserStore();
+
+  const logoutHandler = () => {
+    localStorage.removeItem("access_token");
+    logoutUser();
+    push(ROUTES.HOME_RECOMMENDATION_PAGE);
+  };
 
   return (
     <S.CustomSheet isOpen={logoutModalOpen} onClose={toggleLogoutModal}>
@@ -20,12 +31,12 @@ function LogoutModal() {
               로그아웃 시, 기존 로그인하셨던 SNS로 재로그인이 가능합니다.
             </Text>
           </S.TextContainer>
-          <S.LogoutButton type="button" shape="square" label="logout">
+          <S.LogoutButton type="button" shape="square" label="logout" onClick={logoutHandler}>
             <Text size={16} weight={500} color={colors.grey[100]}>
               로그아웃
             </Text>
           </S.LogoutButton>
-          <S.CancelButton type="button" shape="square" label="later">
+          <S.CancelButton type="button" shape="square" label="later" onClick={toggleLogoutModal}>
             <Text size={14} weight={500} color={colors.grey[700]}>
               다음에 할게요
             </Text>
