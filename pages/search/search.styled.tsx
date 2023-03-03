6fd6f0c67e853dbb4a94e7186247df0e8350aa3c
rachel490 <ccori12@naver.com>
2023-03-03 20:56:35 +0900

    feat: 검색 결과 페이지 UI 구현

--- pages/search/search.styled.tsx
+++ pages/search/search.styled.tsx
@@ -4,3 +4,11 @@ export const Layout = styled.div`
   padding: 0 1.6rem;
   margin-top: 2.4rem;
 `;
+
+export const Grid = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 15.8rem);
+  grid-row-gap: 3.2rem;
+  grid-column-gap: 1.2rem;
+`;
