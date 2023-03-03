9a7f2178ce6c4f16d2c1fe17081319dbe449ef82
rachel490 <ccori12@naver.com>
2023-03-03 19:35:51 +0900

    feat: searchbar에 액션 붙이기

--- components/search/SearchNav/SearchNav.tsx
+++ components/search/SearchNav/SearchNav.tsx
@@ -1,19 +1,52 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
 import { useTheme } from "@emotion/react";
 import { SearchClose } from "@/assets/icons";
+import useSearchStore from "@/store/search";
 import Icon from "@/components/shared/Icon/Icon";
+import Button from "@/components/shared/Button/Button";
 import * as S from "./SearchNav.styled";
 
 function SearchNav() {
   const { colors } = useTheme();
+  const { back: previous } = useRouter();
+  const [searchKeyword, setSearchKeyword] = useState<string>("");
+  const { addSearchHistory, updateCurrentSearch } = useSearchStore();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    addSearchHistory(searchKeyword);
+    updateCurrentSearch(searchKeyword);
+    setSearchKeyword("");
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchKeyword(e.target.value);
+  };
+
+  const deleteKeyword = () => {
+    setSearchKeyword("");
+  };
+
+  const previousPage = () => {
+    previous();
+  };
 
   return (
     <S.Wrap>
-      <Icon name="arrowLeft" size="l" color={colors.grey[700]} />
-      <S.InputWrap>
+      <Button onClick={previousPage} type="button" label="previous page" shape="square">
+        <Icon name="arrowLeft" size="l" color={colors.grey[700]} />
+      </Button>
+      <S.InputForm onSubmit={handleSubmit}>
         <Icon name="search" size="m" color={colors.grey[600]} />
-        <input type="text" placeholder="케이크 디자인 검색하기" />
-        <SearchClose />
-      </S.InputWrap>
+        <input
+          type="text"
+          placeholder="케이크 디자인 검색하기"
+          value={searchKeyword}
+          onChange={handleChange}
+        />
+        <SearchClose onClick={deleteKeyword} />
+      </S.InputForm>
     </S.Wrap>
   );
 }
