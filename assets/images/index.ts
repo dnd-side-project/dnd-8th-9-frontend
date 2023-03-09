62a2f054b466f9007cd339507ef40570c56a4e69
rachel490 <ccori12@naver.com>
2023-03-09 09:01:42 +0900

    style: onboard cake 이미지 추가

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -2,3 +2,4 @@ export { default as SizeCircle } from "./sizeCircle.svg";
 export { default as UploadImage } from "./uploadImage.svg";
 export { default as Cake } from "./cake.svg";
 export { default as DangdoLogo } from "./dangdoLogo.svg";
+export { default as OnboardCake } from "./onboardCake.svg";
