61204842c08731c38b27f1525b3376ab2c7b0820
rachel490 <ccori12@naver.com>
2023-03-13 18:10:36 +0900

    refactor: Icon 컴포넌트 styled로 확장가능하도록 className prop 추가

--- components/shared/Icon/Icon.tsx
+++ components/shared/Icon/Icon.tsx
@@ -6,11 +6,12 @@ interface IProp {
   name: string;
   color?: string;
   fill?: string;
+  className?: string;
 }
 
-function Icon({ size = "s", name, color = "none", fill = "none" }: IProp) {
+function Icon({ size = "s", name, color = "none", fill = "none", className }: IProp) {
   return (
-    <S.IconWrap size={size} color={color} fill={fill}>
+    <S.IconWrap size={size} color={color} fill={fill} className={className}>
       {ICONS[name]}
     </S.IconWrap>
   );
