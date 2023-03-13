e542cf58dcf5743008e5c1b21240c20f2fd1805f
rachel490 <ccori12@naver.com>
2023-03-13 21:42:14 +0900

    feat: ProgressBar 변경된 디자인 적용 및 isPreview에 따라 조건부 스타일 적용

--- components/store/review/ProgressBar/ProgressBar.styled.tsx
+++ components/store/review/ProgressBar/ProgressBar.styled.tsx
@@ -1,49 +1,74 @@
 import styled from "@emotion/styled";
 
-interface IProgress {
-  count: number;
-}
-
-export const Container = styled.div`
+export const StatItem = styled.div`
   display: flex;
   align-items: center;
-  margin: 0.6rem 1.3rem;
-  height: 1.7rem;
-  :first-of-child {
-    margin-top: 2rem;
+`;
+
+export const Bar = styled.div<{ count: number; rank: number }>`
+  background-color: ${({ theme }) => theme.colors.grey[100]};
+  width: 100%;
+  border-radius: 0.8rem;
+  overflow: hidden;
+  flex-grow: 1;
+
+  div {
+    width: ${({ count }) => count}%;
+    height: 100%;
+    border-radius: 0.8rem;
+    background-color: ${({ theme, rank }) => {
+      if (rank === 0) {
+        return theme.colors.pink[700];
+      }
+      if (rank === 1) {
+        return theme.colors.pink[600];
+      }
+      return theme.colors.pink[400];
+    }};
   }
 `;
 
-export const OptionName = styled.div`
-  font-weight: 500;
-  font-size: ${({ theme }) => theme.fontSizes[12]};
-  line-height: 1.6rem;
-  color: ${({ theme }) => theme.colors.black};
-  width: 45%;
-`;
+export const StatContainer = styled.div`
+  &.isPreview {
+    row-gap: 1.6rem;
+    padding: 2rem 1.6rem;
+    background-color: ${({ theme }) => theme.colors.blue[100]};
+    border: none;
 
-export const Background = styled.div`
-  width: 55%;
-  background-color: ${({ theme }) => theme.colors.white[500]};
-  height: 0.6rem;
-  border-radius: 8px;
-`;
+    ${Bar} {
+      background-color: ${({ theme }) => theme.colors.grey[100]};
+      height: 0.8rem;
+    }
 
-export const Progress = styled.div<IProgress>`
-  background-color: ${({ theme }) => theme.colors.navy[400]};
-  width: ${({ count }) => `${count}%`};
-  height: 100%;
-  border-radius: 8px;
+    ${StatItem} {
+      > span:first-child {
+        min-width: 13rem;
+      }
+    }
+  }
+
+  margin-top: 2rem;
+  background-color: ${({ theme }) => theme.colors.grey[100]};
+  padding: 3.2rem 1.6rem;
+  border: 1px solid ${({ theme }) => theme.colors.grey[300]};
+  border-radius: 0.8rem;
   display: flex;
-`;
+  flex-direction: column;
+  row-gap: 1.2rem;
 
-export const Count = styled.div`
-  text-align: right;
-  font-weight: 500;
-  font-size: ${({ theme }) => theme.fontSizes[12]};
-  line-height: 1.6rem;
-  color: ${({ theme }) => theme.colors.gray[200]};
-  width: 25%;
-`;
+  ${StatItem} {
+    > span:first-child {
+      min-width: 10.5rem;
+    }
 
-//
+    > span:last-child {
+      min-width: 3rem;
+      text-align: right;
+    }
+  }
+
+  ${Bar} {
+    background-color: ${({ theme }) => theme.colors.grey[200]};
+    height: 0.6rem;
+  }
+`;
