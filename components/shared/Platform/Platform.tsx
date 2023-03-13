407851cddca6b934bd161585bb5e4300f3380eeb
rachel490 <ccori12@naver.com>
2023-03-13 18:09:22 +0900

    refactor: Platform 컴포넌트에 업데이트된 link 데이터 구조 반영

--- components/shared/Platform/Platform.tsx
+++ components/shared/Platform/Platform.tsx
@@ -1,5 +1,6 @@
-import theme from "@/styles/theme";
 import { useTheme } from "@emotion/react";
+import { ILink } from "@/api/types/shared";
+import theme from "@/styles/theme";
 import Icon from "../Icon/Icon";
 import Text from "../Text/Text";
 import * as S from "./Platform.styled";
@@ -16,9 +17,7 @@ const LINK_ICON: ILinkIcon = {
 };
 
 interface IProp {
-  links: {
-    [key: string]: string;
-  };
+  links: ILink[];
 }
 
 function Platform({ links }: IProp) {
@@ -29,7 +28,7 @@ function Platform({ links }: IProp) {
       <Text weight={500} size={13} color={colors.grey[700]}>
         주문방식
       </Text>
-      {Object.keys(links).map(link => LINK_ICON[link])}
+      {links.map(link => LINK_ICON[link.platform])}
     </S.PlatformWrap>
   );
 }
