25f0615feec8e1d60db496db3cede3eefdcba13d
rachel490 <ccori12@naver.com>
2023-03-13 11:39:49 +0900

    refactor: storeHero안의 컴포넌트 한꺼번에 export하는 파일 추가

---
+++ components/store/StoreHero/index.ts
@@ -0,0 +1,4 @@
+export { default as MainInfo } from "./MainInfo/MainInfo";
+export { default as OrderLink } from "./OrderLink/OrderLink";
+export { default as RecieveMethod } from "./RecieveMethod/RecieveMethod";
+export { default as ReviewInfo } from "./ReviewInfo/ReviewInfo";
