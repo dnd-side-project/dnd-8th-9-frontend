652d5484eec4a77c41507552ab9c9676e4efc280
rachel490 <ccori12@naver.com>
2023-03-13 21:44:46 +0900

    refactor: 스토어의 매력 변경된 디자인 적용

--- components/store/StoreHero/ReviewInfo/ReviewInfo.tsx
+++ components/store/StoreHero/ReviewInfo/ReviewInfo.tsx
@@ -1,48 +1,24 @@
-import { useRouter } from "next/router";
-import Link from "next/link";
 import { useTheme } from "@emotion/react";
-import { getOverallComment } from "@/utils/util";
 import { IStoreReview } from "@/api/types/review";
-import Button from "@/components/shared/Button/Button";
-import Icon from "@/components/shared/Icon/Icon";
-import Tag from "@/components/shared/Tag/Tag";
+import Text from "@/components/shared/Text/Text";
+import ProgressBar from "../../review/ProgressBar/ProgressBar";
 import * as S from "./ReviewInfo.styled";
 
 interface IProp {
   overallStats: IStoreReview["overallStats"];
 }
+
 function ReviewInfo({ overallStats }: IProp) {
-  const { asPath } = useRouter();
+  const { totalReviews, stats } = overallStats;
   const { colors } = useTheme();
-  const { rating, totalReviews, stats } = overallStats;
 
   return (
-    <S.ReviewBox>
-      <h3>당도러가 선택한 이 스토어의 매력 🌟</h3>
-      <S.ReviewStats>
-        <Icon size="m" name="heart" fill={colors.primary} color={colors.primary} />
-        <span>{rating}%</span>
-        <small>({totalReviews})명 참여</small>
-        <strong>{getOverallComment(rating)}</strong>
-      </S.ReviewStats>
-      <S.ReviewTags className="tags">
-        {stats.slice(0, 2).map(({ optionName, count }) => (
-          <Tag type="double" label={optionName} key={optionName} cssProp={S.optionStyle}>
-            <p>{optionName}</p>
-            <span>{count}</span>
-          </Tag>
-        ))}
-        <div className="iconWrap">
-          <Icon name="ellipsis" />
-        </div>
-      </S.ReviewTags>
-      <Link href={`${asPath}/form`}>
-        <Button type="button" label="write a review" shape="square" cssProp={S.reviewButton}>
-          <Icon size="s" name="pencil" color={colors.primary} />
-          <p>리뷰쓰기</p>
-        </Button>
-      </Link>
-    </S.ReviewBox>
+    <S.ReviewContentBox title="이 스토어의 매력">
+      <Text size={13} weight={500} color={colors.grey[700]}>
+        전체 리뷰 {totalReviews}건
+      </Text>
+      <ProgressBar stats={stats} isPreview />
+    </S.ReviewContentBox>
   );
 }
 
