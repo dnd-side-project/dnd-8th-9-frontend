274099bb6b34a1151f234ebca3715330444dbb38
rachel490 <ccori12@naver.com>
2023-03-15 08:59:40 +0900

    refactor: NoticeInfo 불필요한 하위 컴포넌트 제거 및 스타일링 변경

--- components/store/infoTable/NoticeInfo/NoticeInfo.tsx
+++ components/store/infoTable/NoticeInfo/NoticeInfo.tsx
@@ -1,4 +1,6 @@
-import Text from "../Text/Text";
+import { useState } from "react";
+import { useTheme } from "@emotion/react";
+import Icon from "@/components/shared/Icon/Icon";
 import * as S from "./NoticeInfo.styled";
 
 interface INoticeInfo {
@@ -10,9 +12,33 @@ interface INoticeInfo {
 }
 
 export default function NoticeInfo({ data }: INoticeInfo) {
+  const { colors } = useTheme();
+  const [isPreviewMode, setIsPreviewMode] = useState(true);
+
+  const toggleMode = () => {
+    setIsPreviewMode(prev => !prev);
+  };
+
   return (
-    <S.Container>
-      <Text data={data.content} />
-    </S.Container>
+    <S.Content>
+      <S.Date size={12} color={colors.grey[500]}>
+        {data.date}
+      </S.Date>
+      <S.TextBox as="p" isPreviewMode={isPreviewMode}>
+        {data.content}
+      </S.TextBox>
+      <S.FullTextButton
+        onClick={toggleMode}
+        type="button"
+        label={isPreviewMode ? "preview text" : "full text"}
+        shape="round"
+      >
+        {isPreviewMode ? (
+          <Icon name="arrowDown" size="m" color={colors.grey[600]} />
+        ) : (
+          <Icon name="arrowUp" size="m" color={colors.grey[600]} />
+        )}
+      </S.FullTextButton>
+    </S.Content>
   );
 }
