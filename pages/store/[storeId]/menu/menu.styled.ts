bb2e84e2c24aa03463a82b35669de7de57636457
rachel490 <ccori12@naver.com>
2023-03-15 11:44:29 +0900

    style: 메뉴리스트에 margin bottom 추가

--- pages/store/[storeId]/menu/menu.styled.ts
+++ pages/store/[storeId]/menu/menu.styled.ts
@@ -5,6 +5,7 @@ export const ListWrap = styled.div`
 `;
 
 export const CardWrapper = styled.div`
+  margin-bottom: 0.8rem;
   padding-block: 0.8rem;
   border-bottom: 1px solid ${({ theme }) => theme.colors.grey[200]};
 `;
