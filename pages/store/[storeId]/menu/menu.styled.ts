ab5ef7132ca721b17736f2c5c84262030515245d
rachel490 <ccori12@naver.com>
2023-03-15 11:24:55 +0900

    refactor: store menu list를 MenuSingleCard 사용해서 리팩토링

--- pages/store/[storeId]/menu/menu.styled.ts
+++ pages/store/[storeId]/menu/menu.styled.ts
@@ -1,10 +1,10 @@
 import styled from "@emotion/styled";
 
-export const Container = styled.div`
-  position: relative;
-  width: inherit;
-`;
-
 export const ListWrap = styled.div`
   padding: 3.2rem 1.6rem;
+`;
+
+export const CardWrapper = styled.div`
+  padding-block: 0.8rem;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.grey[200]};
 `;
