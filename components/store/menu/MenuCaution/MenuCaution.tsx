20fa2605bf3b7b405fd7d76a8209f1a5a932653e
rachel490 <ccori12@naver.com>
2023-03-15 20:23:07 +0900

    style: 메뉴 상세 주의사항 섹션 디자인 변경사항 적용

--- components/store/menu/MenuCaution/MenuCaution.tsx
+++ components/store/menu/MenuCaution/MenuCaution.tsx
@@ -1,4 +1,5 @@
 import { IMenuOption } from "@/api/types/menu";
+import Text from "@/components/shared/Text/Text";
 import * as S from "./MenuCaution.styled";
 
 interface IProp {
@@ -9,7 +10,9 @@ function MenuCaution({ caution }: IProp) {
   return (
     <S.CautionContent>
       {caution.value.map((cautionValue, idx) => (
-        <li key={(cautionValue as string) + String(idx)}>{cautionValue as string}</li>
+        <S.CautionItem key={(cautionValue as string) + String(idx)}>
+          <Text size={13}>{cautionValue as string}</Text>
+        </S.CautionItem>
       ))}
     </S.CautionContent>
   );
