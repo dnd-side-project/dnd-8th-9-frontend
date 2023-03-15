1fab3a4b8605893f4c56746363f4b1607057c769
rachel490 <ccori12@naver.com>
2023-03-15 10:02:06 +0900

    refactor: Packaging 불필요한 하위 컴포넌트 제거 및 스타일링 변경

--- components/store/infoTable/Packaging/Packaging.tsx
+++ components/store/infoTable/Packaging/Packaging.tsx
@@ -1,4 +1,5 @@
-import TextBox from "../TextBox/TextBox";
+import { useTheme } from "@emotion/react";
+import * as S from "./Packaging.styled";
 
 interface IPackaging {
   data: {
@@ -8,10 +9,23 @@ interface IPackaging {
 }
 
 export default function Packaging({ data }: IPackaging) {
+  const { colors } = useTheme();
+
   return (
     <>
-      {data.map(el => (
-        <TextBox key={el.name} data={el} />
+      {data.map(({ name, value }) => (
+        <S.Content key={name}>
+          <S.SubTitle as="h3" weight={600} color={colors.blue[800]}>
+            {name}
+          </S.SubTitle>
+          <S.List>
+            {value.map(item => (
+              <li key={item}>
+                <S.Value size={13}>{item}</S.Value>
+              </li>
+            ))}
+          </S.List>
+        </S.Content>
       ))}
     </>
   );
