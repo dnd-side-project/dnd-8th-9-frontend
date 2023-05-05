680f8a2162d9980f56ffe67e0c6a0c4a36fdf8bb
rachel490 <ccori12@naver.com>
2023-05-05 16:13:53 +0900

    chore: Tanstack-Query SSR을 위한 Hydration setup

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,9 +1,8 @@
-/* eslint-disable func-names */
-import { ReactElement, ReactNode } from "react";
+import { ReactElement, ReactNode, useState } from "react";
 import { NextPage } from "next";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@emotion/react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { DehydratedState, Hydrate, QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import GlobalStyle from "@/styles/Global";
@@ -11,33 +10,31 @@ import theme from "@/styles/theme";
 import SubLayout from "@/components/shared/Layout/SubLayout";
 import MainLayout from "@/components/shared/Layout/MainLayout";
 
-type NextPageWithLayout = NextPage & {
+type NextPageWithLayout<P, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
 };
 
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
+type AppPropsWithLayout = AppProps<{ dehydratedState: DehydratedState }> & {
+  Component: NextPageWithLayout<{ dehydratedState: DehydratedState }>;
 };
-
-const queryClient = new QueryClient();
 
 if (process.env.NODE_ENV === "development") {
   import("@/mocks");
 }
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
-  const getLayout =
-    Component.getLayout ||
-    function (page) {
-      return <SubLayout>{page}</SubLayout>;
-    };
+  const [queryClient] = useState(() => new QueryClient());
+
+  const getLayout = Component.getLayout ?? (page => <SubLayout>{page}</SubLayout>);
 
   return (
     <ThemeProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
-        <GlobalStyle />
-        <MainLayout>{getLayout(<Component {...pageProps} />)}</MainLayout>
-        <ReactQueryDevtools position="bottom-right" initialIsOpen={false} />
+        <Hydrate state={pageProps.dehydratedState}>
+          <GlobalStyle />
+          <MainLayout>{getLayout(<Component {...pageProps} />)}</MainLayout>
+          <ReactQueryDevtools position="bottom-right" initialIsOpen={false} />
+        </Hydrate>
       </QueryClientProvider>
     </ThemeProvider>
   );
