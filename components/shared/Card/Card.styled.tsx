5519ecd03c3c21bbe8578172e546c24c44380775
rachel490 <ccori12@naver.com>
2023-05-07 08:02:39 +0900

    style: 카드 이미지 objectFit cover로 설정

--- components/shared/Card/Card.styled.tsx
+++ components/shared/Card/Card.styled.tsx
@@ -15,6 +15,10 @@ export const ImageWrap = styled.div`
   aspect-ratio: 1/1;
   border-radius: 0.8rem;
   overflow: hidden;
+
+  img {
+    object-fit: cover;
+  }
 `;
 
 export const Container = styled.div<{
