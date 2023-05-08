73a8af818f39c198cb954466259b170dad562367
rachel490 <ccori12@naver.com>
2023-05-08 23:59:12 +0900

    feat: store 상세 mock data 제거 및 ssr로 렌더

--- pages/store/[storeId]/review/index.page.tsx
+++ pages/store/[storeId]/review/index.page.tsx
@@ -1,5 +1,10 @@
-import { reviews as MockReviews } from "@/mocks/mockData/review";
-// import useReviewStore from "@/store/review";
+import { useRouter } from "next/router";
+import { QueryClient, dehydrate } from "@tanstack/react-query";
+import { IStoreDetailsSpecific } from "@/mocks/mockData/store";
+import { useGetStore, useGetStoreReviews } from "@/hooks/queries/store";
+import storeApi from "@/api/domains/store";
+import { storeQueryKey } from "@/constants/queryKey";
+
 import PreviewImage from "@/components/store/review/PreviewImage/PreviewImage";
 import ReviewFilter from "@/components/ReviewFilter/ReviewFilter";
 import OverallStats from "@/components/store/review/OverallStats/OverallStats";
@@ -9,23 +14,31 @@ import Sort from "@/components/shared/Sort/Sort";
 import * as S from "./review.styled";
 
 function ReviewPage() {
-  const { rating, totalReviews, stats } = MockReviews.overallStats;
-  const reviewImages = MockReviews.reviewList.map(review => review.reviewImages).flat();
-  // const { reviews } = useReviewStore();
+  const router = useRouter();
+  const { storeId } = router.query;
+
+  const { data: storeReviewsData } = useGetStoreReviews(Number(storeId));
+  const { data: storeDetailsData } = useGetStore(Number(storeId));
+
+  if (!storeReviewsData || !storeDetailsData) return <h1>Loading...</h1>;
+
+  const reviewImages = storeReviewsData.data.map(review => review.reviewImages).flat();
 
   return (
     <SectionLayout>
       <S.ContentWrap>
-        <OverallStats rating={rating} totalReviews={totalReviews} stats={stats} />
+        {(storeDetailsData.data as IStoreDetailsSpecific).overallStats && (
+          <OverallStats data={(storeDetailsData.data as IStoreDetailsSpecific).overallStats} />
+        )}
         <PreviewImage reviewImages={reviewImages} />
         <S.ReviewContent>
           <S.ReviewTitle as="p" size={16} weight={600}>
-            전체 <strong>{MockReviews.reviewList.length}</strong>건
+            전체 <strong>{storeReviewsData.data.length}</strong>건
           </S.ReviewTitle>
           <ReviewFilter />
           <Sort />
           <div>
-            {MockReviews.reviewList.map(review => (
+            {storeReviewsData.data.map(review => (
               <Review key={review.id} review={review} />
             ))}
           </div>
@@ -36,3 +49,21 @@ function ReviewPage() {
 }
 
 export default ReviewPage;
+
+export async function getServerSideProps({ params: { storeId } }: { params: { storeId: string } }) {
+  const queryClient = new QueryClient();
+  await Promise.all([
+    queryClient.prefetchQuery(storeQueryKey.reviews(Number(storeId)), () =>
+      storeApi.getStoreReviews(Number(storeId)),
+    ),
+    queryClient.prefetchQuery(storeQueryKey.detail(Number(storeId)), () =>
+      storeApi.getStoreDetails(Number(storeId)),
+    ),
+  ]);
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+}
