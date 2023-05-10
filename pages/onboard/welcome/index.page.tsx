21f4c0c08a7aa0469f7c1d049c44d658d0486ea7
rachel490 <ccori12@naver.com>
2023-05-10 19:24:53 +0900

    feat: 온보딩 welcome 페이지 유저 정보 백엔드 api 연결

--- pages/onboard/welcome/index.page.tsx
+++ pages/onboard/welcome/index.page.tsx
@@ -1,21 +1,21 @@
 import Link from "next/link";
 import { useTheme } from "@emotion/react";
 import { OnboardCake } from "@/assets/images";
-import useUserStore from "@/store/user";
 import { ROUTES } from "@/constants/routes";
+import { useGetUser } from "@/hooks/queries/user";
 import Button from "@/components/shared/Button/Button";
 import Text from "@/components/shared/Text/Text";
 import * as S from "./welcome.styled";
 
 function WelcomePage() {
   const { colors } = useTheme();
-  const { userName } = useUserStore();
+  const { data: userData } = useGetUser();
 
   return (
     <S.Wrap>
       <S.TextContainer>
         <Text as="h1" weight={700} size={26} color={colors.grey[800]}>
-          {userName}님, <br />
+          {userData?.data.nickname}님, <br />
           가입을 축하드려요
         </Text>
         <Text as="p" weight={400} size={15} color={colors.grey[800]}>
