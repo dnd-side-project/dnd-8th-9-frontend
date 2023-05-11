ffb798c1aee1133dd1f671db29a744ad96369368
rachel490 <ccori12@naver.com>
2023-05-11 21:29:50 +0900

    style: store 주문양식 스타일 수정

--- pages/store/[storeId]/order/order.styled.ts
+++ pages/store/[storeId]/order/order.styled.ts
@@ -9,40 +9,46 @@ export const Container = styled.div`
   margin-top: 3.2rem;
 `;
 
-export const FormBox = styled.div`
+export const FormBox = styled.form`
   display: flex;
   flex-direction: column;
   padding: 2.4rem 2rem;
-
   border: 1.6px solid ${({ theme }) => theme.colors.grey[300]};
   border-radius: 8px;
+  width: 100%;
+  row-gap: 2rem;
 
-  width: 32.8rem;
-  gap: 2rem;
+  :focus-within {
+    border-color: ${({ theme }) => theme.colors.blue[500]};
+  }
 `;
 
 export const FormCell = styled.div`
   display: flex;
   gap: 1.6rem;
 
+  input::placeholder,
+  input {
+    font-size: ${({ theme }) => theme.fontSizes[13]};
+    font-weight: 500;
+    line-height: 1.6rem;
+  }
+
   input {
     overflow-wrap: break-word;
+    color: ${({ theme }) => theme.colors.grey[800]};
+    width: calc(100% - 10rem);
   }
 
   input::placeholder {
-    height: 1.6rem;
-
-    font-size: ${({ theme }) => theme.fontSizes[13]};
     font-weight: 400;
-    line-height: 1.6rem;
+    width: 100%;
     color: ${({ theme }) => theme.colors.grey[500]};
   }
 `;
 
 export const Title = styled.div`
   width: 8rem;
-  height: 1.7rem;
-
   font-size: ${({ theme }) => theme.fontSizes[14]};
   font-weight: 600;
   line-height: 1.7rem;
