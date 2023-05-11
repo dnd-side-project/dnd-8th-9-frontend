98b57a6ea73a94e3d32ff6369e438f2d312e9af4
rachel490 <ccori12@naver.com>
2023-05-11 20:11:19 +0900

    chore: document 파일 추가

---
+++ pages/_document.page.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="ko">
+      <Head>
+        <title>Dangdo</title>
+        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
