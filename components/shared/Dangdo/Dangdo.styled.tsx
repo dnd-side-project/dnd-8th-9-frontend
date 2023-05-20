8958961e0ff17247b88220a8eb6439c5788d0a66
rachel490 <ccori12@naver.com>
2023-05-21 01:12:36 +0900

    refactor: Dangdo 컴포넌트 size prop 옵션 xl 추가

--- components/shared/Dangdo/Dangdo.styled.tsx
+++ components/shared/Dangdo/Dangdo.styled.tsx
@@ -4,18 +4,20 @@ import Text from "../Text/Text";
 export const TEXT_SIZE = {
   s: "12px",
   m: "14px",
-  l: "20px",
+  l: "16px",
+  xl: "20px",
 };
 
 export const Percent = styled(Text)``;
 
-export const Container = styled.div<{ size: "s" | "m" | "l" }>`
+export const Container = styled.div<{ size: "s" | "m" | "l" | "xl" }>`
   display: flex;
   align-items: center;
-  column-gap: ${({ size }) => (size === "l" ? "0.4rem" : "0.2rem")};
+  column-gap: ${({ size }) => (size === "xl" ? "0.4rem" : "0.2rem")};
 
   ${Percent} {
-    font-weight: ${({ size }) => (size === "l" ? 700 : 600)};
+    font-weight: ${({ size }) => (size === "xl" ? 700 : 600)};
     font-size: ${({ size }) => TEXT_SIZE[size]};
+    padding-top: ${({ size }) => size === "l" && "0.3rem"};
   }
 `;
--- components/shared/Dangdo/Dangdo.tsx
+++ components/shared/Dangdo/Dangdo.tsx
@@ -2,14 +2,14 @@ import Icon from "../Icon/Icon";
 import * as S from "./Dangdo.styled";
 
 interface IProp {
-  size?: "s" | "m" | "l";
+  size?: "s" | "m" | "l" | "xl";
   dangdo: number;
 }
 
 function Dangdo({ size = "s", dangdo }: IProp) {
   return (
     <S.Container size={size}>
-      <Icon name="heart" size={size} />
+      <Icon name="heart" size={size === "xl" ? "l" : size} />
       <S.Percent>
         {size === "l" ? "당도 " : ""}
         {String(dangdo)}%
