c343b2cf3a210b07f35515f9a5798fa251261013
rachel490 <ccori12@naver.com>
2023-05-20 20:48:06 +0900

    feat: 공지사항에 영업시간 table 추가

--- pages/store/[storeId]/notice/index.page.tsx
+++ pages/store/[storeId]/notice/index.page.tsx
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router";
 import { QueryClient, dehydrate } from "@tanstack/react-query";
 import { useGetStore } from "@/hooks/queries/store";
-import { IInfo, INotice } from "@/types/api";
+import { IKeyValue, IInfo, INotice } from "@/types/api";
 import { storeQueryKey } from "@/constants/queryKey";
 import { store } from "@/mocks/mockData/store";
 
@@ -13,11 +13,20 @@ import * as S from "./notice.styled";
 
 const NOTICE_MOCK = store.notice;
 const INFO_MOCK = store.info;
+const BUSINESS_HOURS_MOCK = store.businessHours;
 
-const INFO_TAB_DATA = (notice: INotice | null, info: IInfo | null) => [
+const INFO_TAB_DATA = (
+  notice: INotice | null,
+  info: IInfo | null,
+  businessHours: IKeyValue | null,
+) => [
   {
     title: "업체 공지",
     children: <NoticeInfo data={(notice || NOTICE_MOCK)?.noticeInfo} />,
+  },
+  {
+    title: "영업 시간",
+    children: <Table option="notice" tableData={businessHours || BUSINESS_HOURS_MOCK} />,
   },
   {
     title: "픽업 가능 시간",
@@ -46,11 +55,11 @@ function NoticePage() {
   if (isLoading) return <h1>Loading...</h1>;
   if (isError) return <h1>Error...</h1>;
 
-  const { notice, info } = storeDetailsData.data;
+  const { notice, info, businessHours } = storeDetailsData.data;
 
   return (
     <SectionLayout>
-      {INFO_TAB_DATA(notice, info)?.map(section => (
+      {INFO_TAB_DATA(notice, info, businessHours)?.map(section => (
         <S.InfoContentBox key={section.title} title={section.title}>
           {section.children}
         </S.InfoContentBox>
