70e82b99e92356fd22818d58b3750b03c9ab40e0
rachel490 <ccori12@naver.com>
2023-05-21 01:03:31 +0900

    refactor: 리뷰 작성 - 메뉴 선택 리팩토링

--- components/ReviewForm/SelectMenu/SelectMenu.tsx
+++ components/ReviewForm/SelectMenu/SelectMenu.tsx
@@ -1,18 +1,20 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-import { useState, useEffect } from "react";
-import { useFormMenuStore, useButtonDisabledStore } from "@/store/ReviewForm";
-import MenuCard from "@/components/shared/MenuCard/MenuCard";
 import { menuList } from "@/mocks/mockData/menuList";
+import useReviewStore from "@/store/review";
+import { IStoreMenuListItem } from "@/types/api";
+import MenuCard from "@/components/shared/MenuCard/MenuCard";
 import * as S from "./SelectMenu.styed";
 
 export default function SelectMenu() {
-  const [isClicked, setIsClicked] = useState(0);
-  const { setMenu } = useFormMenuStore(state => state);
-  const { setButtonDisabled, setButtonAbled } = useButtonDisabledStore(state => state);
+  const { updateReviewState, reviewState } = useReviewStore();
 
-  useEffect(() => {
-    setButtonDisabled();
-  }, [setButtonAbled, setButtonDisabled]);
+  const handleClick = (menu: IStoreMenuListItem) => {
+    const { id, storeId, storeName, menuImages, name } = menu;
+    updateReviewState("menuId", id);
+    updateReviewState("menuName", name);
+    updateReviewState("menuImage", menuImages[0].url);
+    updateReviewState("storeId", storeId);
+    updateReviewState("storeName", storeName);
+  };
 
   return (
     <S.Container>
@@ -20,12 +22,8 @@ export default function SelectMenu() {
       {menuList.map(menu => (
         <S.MenuCardClick
           key={menu.id}
-          onClick={() => {
-            setIsClicked(menu.id);
-            setMenu(menu);
-            setButtonAbled();
-          }}
-          className={isClicked === menu.id ? "isActive" : "isNotActive"}
+          onClick={() => handleClick(menu)}
+          className={reviewState.menuId === menu.id ? "isActive" : "isNotActive"}
         >
           <MenuCard menu={menu} />
         </S.MenuCardClick>
