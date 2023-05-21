934dd69977a984a0fc5cc7e6020d4e6b62d7994f
rachel490 <ccori12@naver.com>
2023-05-21 18:45:46 +0900

    style: 메뉴 선택 최신 디자인 반영하여 UI 수정

--- components/ReviewForm/SelectMenu/SelectMenu.tsx
+++ components/ReviewForm/SelectMenu/SelectMenu.tsx
@@ -1,11 +1,22 @@
-import { menuList } from "@/mocks/mockData/menuList";
+/* eslint-disable @typescript-eslint/no-shadow */
+import { useRouter } from "next/router";
+import { useTheme } from "@emotion/react";
 import useReviewStore from "@/store/review";
 import { IStoreMenuListItem } from "@/types/api";
-import MenuCard from "@/components/shared/MenuCard/MenuCard";
+import { useGetStore, useGetStoreMenus } from "@/hooks/queries/store";
+import Icon from "@/components/shared/Icon/Icon";
+import MenuSingleCard from "@/components/shared/Card/MenuSingleCard";
 import * as S from "./SelectMenu.styed";
 
 export default function SelectMenu() {
+  const { colors } = useTheme();
+  const {
+    query: { storeId },
+  } = useRouter();
+
   const { updateReviewState, reviewState } = useReviewStore();
+  const { data: storeDetailsData } = useGetStore(Number(storeId));
+  const { data: storeMenusData } = useGetStoreMenus(Number(storeId));
 
   const handleClick = (menu: IStoreMenuListItem) => {
     const { id, storeId, storeName, menuImages, name } = menu;
@@ -17,17 +28,28 @@ export default function SelectMenu() {
   };
 
   return (
-    <S.Container>
-      <S.FormTitle>주문하신 메뉴는 무엇인가요?</S.FormTitle>
-      {menuList.map(menu => (
-        <S.MenuCardClick
-          key={menu.id}
-          onClick={() => handleClick(menu)}
-          className={reviewState.menuId === menu.id ? "isActive" : "isNotActive"}
-        >
-          <MenuCard menu={menu} />
-        </S.MenuCardClick>
-      ))}
-    </S.Container>
+    <div>
+      <S.FormTitle as="h1" size={18} weight={600}>
+        {storeDetailsData?.data.name}에서 <br /> 주문하신 메뉴를 선택해주세요.
+      </S.FormTitle>
+      <S.MenuList>
+        {storeMenusData?.data.map(menu => (
+          <S.MenuCardContainer
+            key={menu.id}
+            onClick={() => handleClick(menu)}
+            className={reviewState.menuId === menu.id ? "isActive" : "isNotActive"}
+          >
+            <S.IconWrap>
+              <Icon
+                name="check"
+                size="s"
+                color={reviewState.menuId === menu.id ? colors.grey[100] : colors.grey[500]}
+              />
+            </S.IconWrap>
+            <MenuSingleCard mode="none" size="s" data={menu} />
+          </S.MenuCardContainer>
+        ))}
+      </S.MenuList>
+    </div>
   );
 }
