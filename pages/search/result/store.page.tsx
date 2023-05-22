befc72da7faa7ec29ec0ac75777b02dbc2a919be
rachel490 <ccori12@naver.com>
2023-05-23 01:25:04 +0900

    feat: getStoreList에 option prop 추가 및 search result store 페이지에 적용

--- pages/search/result/store.page.tsx
+++ pages/search/result/store.page.tsx
@@ -1,77 +1,43 @@
-import { useEffect, useState } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import { resultTab } from "@/constants/tabs";
-import { storeList } from "@/mocks/mockData/storeList";
-import useSearchStore from "@/store/search";
-import useFilterStore from "@/store/filter";
-import { IStoreListItem, TCategory } from "@/types/api";
-
+import { useGetStoreList } from "@/hooks/queries/store";
 import SearchNav from "@/components/search/SearchNav/SearchNav";
 import StoreDoubleCard from "@/components/shared/Card/StoreDoubleCard";
 import FilterBar from "@/components/shared/FilterBar/FilterBar";
 import FilterModal from "@/components/shared/FilterModal/FilterModal";
 import Sort from "@/components/shared/Sort/Sort";
 import Tab from "@/components/shared/Tab/Tab";
-import { IFilterStore } from "@/types/store/filter";
+import NoResult from "@/components/search/NoResult/NoResult";
 import * as S from "../search.styled";
 
 function SearchResultStorePage() {
-  const { currentSearch } = useSearchStore();
-  const { selectedFilterOptions } = useFilterStore();
-  const [searchResult, setSearchResult] = useState<IStoreListItem[]>([]);
-  const [filterResult, setFilterResult] = useState<IStoreListItem[]>([]);
-  const [data, setData] = useState<IStoreListItem[]>(searchResult || filterResult || []);
+  const {
+    query: { key },
+  } = useRouter();
+  const { data: storesData, refetch } = useGetStoreList({ search: key as string });
 
   useEffect(() => {
-    const result = storeList.filter(store => store.name.includes(currentSearch));
-    setSearchResult(result);
-    setData(result);
-  }, [currentSearch]);
-
-  useEffect(() => {
-    if (
-      Object.keys(selectedFilterOptions).some(
-        filter => selectedFilterOptions[filter as keyof IFilterStore].length !== 0,
-      )
-    ) {
-      const result = searchResult.filter(store => {
-        return selectedFilterOptions["카테고리"].map(category => {
-          return store.category.includes(category);
-        });
-      });
-      setFilterResult(result);
-      setData(result);
-    }
-  }, [searchResult, selectedFilterOptions]);
-
-  useEffect(() => {
-    if (
-      Object.keys(selectedFilterOptions).some(
-        filter => selectedFilterOptions[filter as keyof IFilterStore].length !== 0,
-      )
-    ) {
-      const result = searchResult.filter(store => {
-        return selectedFilterOptions["카테고리"].some(category => {
-          return store.category.includes(category);
-        });
-      });
-
-      setFilterResult(result);
-      setData(result);
-    }
-  }, [searchResult, selectedFilterOptions]);
+    refetch();
+  }, [key]);
 
   return (
     <S.Layout>
-      <SearchNav mode="result" />
-      <Tab menuList={resultTab} target="resultTab" />
-      <Sort />
-      <FilterBar />
-      <S.StoreGrid>
-        {data.map(store => (
-          <StoreDoubleCard key={store.id} data={store} />
-        ))}
-      </S.StoreGrid>
-      <FilterModal />
+      <SearchNav mode="store" />
+      {!storesData?.data.length ? (
+        <NoResult currentSearch={key as string} />
+      ) : (
+        <>
+          <Tab menuList={resultTab} target="resultTab" />
+          <Sort />
+          <FilterBar />
+          {storesData?.data.map(store => (
+            <StoreDoubleCard key={store.id} data={store} />
+          ))}
+          <FilterModal />
+        </>
+      )}
     </S.Layout>
   );
 }
