1dfa053291301e43f3dddf3db1ba4ddadac32e10
Rachel Shin (dwell) <72786354+rachel490@users.noreply.github.com>
2023-06-09 00:20:14 +0900

    Feat: PWA 만들기 (#180)
    * chore: next-pwa 설치
    
    * feat: pwa에 필요한 manifest.json & icon 추가
    
    * chore: next.config에 pwa 설정 추가
    
    * fix: pwa theme color 변경
    
    * chore: pwa 관련 gitignore 파일들 추가
    
    * refactor: next.config 파일 코드 리팩토링

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV !== "development",
+});
+
 const nextConfig = {
   pageExtensions: ["page.ts", "page.tsx"],
   reactStrictMode: true,
@@ -45,4 +53,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
