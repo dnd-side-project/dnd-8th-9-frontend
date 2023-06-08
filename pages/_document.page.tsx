e6dc3cfd4058a8cf4f3e03eeaaf5f64fb32e8020
rachel490 <ccori12@naver.com>
2023-06-08 23:09:57 +0900

    feat: pwa에 필요한 manifest.json & icon 추가

--- pages/_document.page.tsx
+++ pages/_document.page.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png" />
+        <meta name="theme-color" content="#F45C65" />
+      </Head>
       <body>
         <Main />
         <NextScript />
