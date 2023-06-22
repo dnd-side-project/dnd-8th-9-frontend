74d7e08068304f0993cb9e17d9b573dcf86ca0c9
rachel490 <ccori12@naver.com>
2023-06-23 02:43:52 +0900

    feat: favicon 추가 및 viewport meta tag 추가

--- pages/_document.page.tsx
+++ pages/_document.page.tsx
@@ -5,8 +5,13 @@ export default function Document() {
     <Html lang="ko">
       <Head>
         <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/icon.png" />
         <meta name="theme-color" content="#fff" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0,viewport-fit=cover"
+        />
       </Head>
       <body>
         <Main />
