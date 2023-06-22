6b0f1dfe8202087d0ee713b66ffc83aa945e379c
rachel490 <ccori12@naver.com>
2023-06-23 03:39:13 +0900

    feat: favicon 재설정

--- pages/_document.page.tsx
+++ pages/_document.page.tsx
@@ -5,8 +5,11 @@ export default function Document() {
     <Html lang="ko">
       <Head>
         <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#f34d57" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
         <meta name="theme-color" content="#fff" />
         <meta
           name="viewport"
